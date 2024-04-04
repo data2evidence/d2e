f7f20d2601751835d1b077a377f1804c1d0c5301
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2024-04-04 11:20:47 +0800

    Update V1.0.0.0.3__drop_schema_metadata_table.sql

--- services/alp-db-svc/db/migrations/postgres/changesets/schemaMetadata/V1.0.0.0.3__drop_schema_metadata_table.sql
+++ services/alp-db-svc/db/migrations/postgres/changesets/schemaMetadata/V1.0.0.0.3__drop_schema_metadata_table.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
---changeset alp:V1.0.0.0.3__drop_schema_metadata_table.sql
+--changeset alp:V1.0.0.0.3__drop_schema_metadata_table
 
 DROP TABLE IF EXISTS schema_metadata CASCADE;
