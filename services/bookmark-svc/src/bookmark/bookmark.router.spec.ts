990dc845f8c32e84549d0d37d82892a3abc0d80f
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-04-04 13:21:30 +0800

    switch to jest

---
+++ services/bookmark-svc/src/bookmark/bookmark.router.spec.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import { BookmarkRouter } from './bookmark.router'
+
+describe('BookmarkRouter', () => {
+  let router: Router
+
+  beforeAll(() => {
+    router = new BookmarkRouter().getRouter()
+  })
+
+  describe('has routes', () => {
+    const routes = [
+      { path: '/', method: 'get' },
+      { path: `/`, method: 'post' },
+      { path: '/:bookmarkId', method: 'put' },
+      { path: '/:bookmarkId', method: 'delete' },
+      { path: `/bookmarkIds`, method: 'delete' },
+    ]
+
+    test.each(routes)('`$method` exists on $path', route => {
+      expect(router.stack.some(s => Object.keys(s.route.methods).includes(route.method))).toBe(true)
+      expect(router.stack.some(s => s.route.path === route.path)).toBe(true)
+    })
+  })
+})
