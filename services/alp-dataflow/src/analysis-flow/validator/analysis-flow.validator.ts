5eb8e6f708d0cab3652c20a1904a40b6ad68eac1
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-04-09 12:52:38 +0800

    fix dataflow unit test

--- services/alp-dataflow/src/analysis-flow/validator/analysis-flow.validator.ts
+++ services/alp-dataflow/src/analysis-flow/validator/analysis-flow.validator.ts
@@ -31,7 +31,7 @@ export class IsDataflowNameExistConstraint implements ValidatorConstraintInterfa
 }
 
 export function IsDataflowNameExist(options?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
+  return function (object: object, propertyName: string) {
     registerDecorator({
       target: object.constructor,
       propertyName,
