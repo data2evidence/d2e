db652e6ca14fff187001bac9bf7ada53ae913b97
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-12 17:57:22 +0800

    TLS_REGENERATE restart

--- scripts/gen-tls.sh
+++ scripts/gen-tls.sh
@@ -15,22 +15,24 @@ touch ${DOTENV_FILE}
 CONTAINER_CRT_DIR=/data/caddy/certificates/$TLS_CA_NAME/wildcard_.$DOMAIN_NAME
 CONTAINER_CA_DIR=/data/caddy/pki/authorities/$TLS_CA_NAME
 
-# remove existing certificates
-[ ${TLS_REGENERATE} = true ] && docker volume inspect $VOLUME_NAME > /dev/null && docker run --rm -v $VOLUME_NAME:/volume -w /volume busybox rm -rf /volume/caddy/certificates/$TLS_CA_NAME/wildcard_.$DOMAIN_NAME
-# start caddy container, if not started
-# docker container inspect -f '{{.State.Status}}' $CONTAINER_NAME 
+if [ ${TLS_REGENERATE} = true ]; then
+	echo ". TLS_REGENERATE remove existing"
+	docker volume inspect $VOLUME_NAME > /dev/null && docker run --rm -v $VOLUME_NAME:/volume -w /volume busybox rm -rf /volume/caddy/certificates/$TLS_CA_NAME/wildcard_.$DOMAIN_NAME
+fi
+
+# start if not already started
 if [ "$( docker container inspect -f '{{.State.Status}}' $CONTAINER_NAME 2> /dev/null )" != "running" ]; then
-	yarn base:minerva --env-file $DOTENV_FILE up $CONTAINER_NAME --wait 2>&1 | grep -vE "WARN\[0000\]|is not set"
+	yarn base:minerva --env-file $DOTENV_FILE up $CONTAINER_NAME --wait 2>&1 | grep -vE 'WARN[0000]|is not set'
 	if [ "$( docker container inspect -f '{{.State.Status}}' $CONTAINER_NAME 2> /dev/null )" != "running" ]; then
 		echo FATAL: $CONTAINER_NAME failed to start
 		docker container inspect --format json -f '{{.State}}' $CONTAINER_NAME
 		docker container inspect -o json 
 		exit 1
 	fi
-	sleep 3
+# restart if TLS_REGENERATE
+elif [ ${TLS_REGENERATE} = true ]; then
+	yarn base:minerva --env-file $DOTENV_FILE restart $CONTAINER_NAME 2>&1 | grep -vE 'WARN[0000]|is not set'
 fi
-# restart to re-generate certificate
-# [ ${TLS_REGENERATE} = true ] && yarn base:minerva --env-file $DOTENV_FILE restart $CONTAINER_NAME 2> /dev/null
 
 # remove existing certs from dotenv
 for VAR_NAME in TLS__INTERNAL__CA_CRT TLS__INTERNAL__CRT TLS__INTERNAL__KEY; do sed -i.bak "/$VAR_NAME=/,/END CERTIFICATE-----'/d" $DOTENV_FILE; done
