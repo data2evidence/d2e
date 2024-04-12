2ea1f890444a3a7cf2b298aed44df33276403343
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-12 19:51:53 +0800

    update

--- internal/scripts/backup/volumes/backup.sh
+++ internal/scripts/backup/volumes/backup.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-# vackup all volumes
-docker volume ls | grep alp_
+# vackup all docker volumes
+
+GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
+cd $GIT_BASE_DIR/cache/vackup
 
 DATETIME_STAMP=$(date +%Y%m%dT%H%M)Z;
 echo DATETIME_STAMP=$DATETIME_STAMP
+echo
+
+docker volume ls | grep alp_
+echo
+
 VOLUMES=($(docker volume ls -q | grep alp_))
-for VOLUME in ${VOLUMES[@]}; do 
+for VOLUME in ${VOLUMES[@]}; do
+  SECONDS=0
   vackup export $VOLUME $DATETIME_STAMP.$VOLUME.tar.zst
+  echo $FILE in $(($SECONDS/60)) minutes | tee -a private-$DATETIME_STAMP.log
+  echo
 done
--- internal/scripts/backup/volumes/restore.sh
+++ internal/scripts/backup/volumes/restore.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+# restore docker volumes from timestamp
+
+# inputs
 DATETIME_STAMP=$1
+
+# vars
 COMPOSE_PROJECT_NAME=alp
 COMPOSE_VERSION=$(docker compose version | awk '{print $NF}' | awk -F- '{print $1}' | sed -e 's/^v//'); echo COMPOSE_VERSION=$COMPOSE_VERSION
-SECONDS=0
-cd /alp-backup
+GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
+cd $GIT_BASE_DIR/cache/vackup
 
+# action
 FILES=($(ls $DATETIME_STAMP*.zst | grep -Ev 'alp_caddy'))
 for FILE in ${FILES[@]}; do 
+  SECONDS=0
   COMPOSE_VOLUME_NAME=$(echo $FILE | awk -F. '{print $3}')
   echo docker volume rm $COMPOSE_VOLUME_NAME
   echo docker volume create --label com.docker.compose.project=$COMPOSE_PROJECT_NAME --label=com.docker.compose.version=$COMPOSE_VERSION --label com.docker.compose.volume=$VOLUME_NAME $COMPOSE_VOLUME_NAME
   CMD=(vackup import $FILE $COMPOSE_VOLUME_NAME)
   echo . ${CMD[@]}
   ${CMD[@]}
-  echo $FILE in $(($SECONDS/60)) minutes | tee private-$DATETIME_STAMP.log
+  echo $FILE in $(($SECONDS/60)) minutes | tee -a private-$DATETIME_STAMP.log
   echo
 done
