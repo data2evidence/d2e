ff5a4a9aa9dd7935054c1fd87e0eb32652e1fec4
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-12 18:03:16 +0800

    fix setup

--- internal/scripts/setup.sh
+++ internal/scripts/setup.sh
@@ -6,5 +6,6 @@ GIT_BASE_DIR=$(git rev-parse --show-toplevel)
 # expected OP_VAULT_NAME set e.g. in .zshrc
 [ -z "${OP_VAULT_NAME}" ] && echo 'FATAL ${OP_VAULT_NAME} is required' && exit 1
 
-op read --out-file $GIT_BASE_DIR/cache/drivers/alp-dbcli-v1.0.0.tgz "op://${OP_VAULT_NAME}/cache-alp-dbcli-v1.0.0.tgz/alp-dbcli-v1.0.0.tgz"
-op read --out-file $GIT_BASE_DIR/cache/drivers/ngdbc-latest.jar "op://${OP_VAULT_NAME}/cache-ngdbc-latest/ngdbc-latest.jar"
+DRIVERS_DIR=$GIT_BASE_DIR/cache/drivers
+op read --out-file $DRIVERS_DIR/alp-dbcli-v1.0.0.tgz "op://${OP_VAULT_NAME}/cache-alp-dbcli-v1.0.0.tgz/alp-dbcli-v1.0.0.tgz" --force
+op read --out-file $DRIVERS_DIR/ngdbc-latest.jar "op://${OP_VAULT_NAME}/cache-ngdbc-latest/ngdbc-latest.jar" --force
