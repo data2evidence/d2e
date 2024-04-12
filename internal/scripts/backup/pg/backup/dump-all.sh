c7d9629c057d5374384186ac7764fc50bbb400b3
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-12 19:57:39 +0800

    permissions

--- internal/scripts/backup/pg/backup/dump-all.sh
+++ internal/scripts/backup/pg/backup/dump-all.sh
@@ -5,4 +5,5 @@ CONTAINER_NAME=alp-minerva-postgres-1
 DATETIME_STAMP=$(date +%Y%m%dT%H%M)Z
 BACKUP_DIR=/backup
 BACKUP_FILE=$BACKUP_DIR/$DATETIME_STAMP.all.sql.gz
-docker exec -it $CONTAINER_NAME sh -c "pg_dumpall --verbose -U postgres | gzip > $BACKUP_FILE; ls -lh $BACKUP_FILE"
+docker exec -it $CONTAINER_NAME sh -c "pg_dumpall -U postgres | gzip > $BACKUP_FILE; ls -lh $BACKUP_FILE"
+# optionally add: --verbose 
