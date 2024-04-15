6470a06f9b87942e4025297b7cd226e1f08f9bfa
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-15 19:19:25 +0800

    fix

--- internal/scripts/gha-mask.sh
+++ internal/scripts/gha-mask.sh
@@ -4,13 +4,13 @@ set -o nounset
 set -o errexit
 
 GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
+DC_YMLS=($(ls $GIT_BASE_DIR/docker-compose*.yml))
 
 # allow level-1 FQDN yml keys as non-sensitive
 export KEYS_ALLOW="CADDY__ALP__PUBLIC_FQDN|CADDY__CFUAA_IDP__PUBLIC_FQDN|IDP__BASE_URL"
 # mask secrets as sensitive
 STRINGS_MASK="password|host|secret|http|PRIVATE"
 # allow container names as non-sensitive
-DC_YMLS=($(ls "$GIT_BASE_DIR/docker-compose*.yml"))
 STRINGS_ALLOW=$(echo $(yq eval-all -N '.services | to_entries | .[] | .key' ${DC_YMLS[@]} | sort -u) $(yq eval-all -N '.services[].container_name' ${DC_YMLS[@]} | grep -vE 'null' | sed -e 's/${BASE_PORT}/1/') | sed -e 's/ /|/g')
 echo STRINGS_ALLOW=${STRINGS_ALLOW}
 
