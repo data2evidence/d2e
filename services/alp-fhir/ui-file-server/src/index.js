e171918c925b16c7a28a7947fb08fda3cd3de0a7
santan maddi <saisantan2004@gmail.com>
2024-04-15 15:39:33 +0800

    WIP alp-fhir frontend

--- services/alp-fhir/ui-file-server/src/index.js
+++ services/alp-fhir/ui-file-server/src/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/ui-files', express.static('ui-files'))
-app.use('/', (req) => 'Hello World')
+app.use('/', express.static('ui-files/dist'))
 
 app.listen(port, () => {
   console.log(`Local UI File Server listening on port ${port}`)
