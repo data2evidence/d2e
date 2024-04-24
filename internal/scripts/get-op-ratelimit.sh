608debd1206ea10cf8c565dab802909f1cbd20de
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-24 17:20:28 +0800

    test

--- internal/scripts/get-op-ratelimit.sh
+++ internal/scripts/get-op-ratelimit.sh
@@ -6,9 +6,10 @@ OP_FORMAT=json
 
 START_JSON_FILE=private.op-ratelimit-start.json
 END_JSON_FILE=private.op-ratelimit-start.json
-echo GITHUB_JOB=$GITHUB_JOB
-echo GITHUB_RUN_ID=$GITHUB_RUN_ID
 
-op service-account ratelimit > 
+[ -z GITHUB_JOB ] || echo FATAL GITHUB_JOB is not set && exit 1
+[ -z GITHUB_RUN_ID ] || echo FATAL GITHUB_RUN_ID is not set && exit 1
 
-echo $GITHUB_CONTEXT | yq '.job'
+# op service-account ratelimit > 
+
+# echo $GITHUB_CONTEXT | yq '.job'
