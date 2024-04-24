4c6a09b68a510b51619a95ac7f94ea820a22b91a
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-24 17:36:11 +0800

    test

--- internal/scripts/get-op-ratelimit.sh
+++ internal/scripts/get-op-ratelimit.sh
@@ -7,13 +7,21 @@ OP_FORMAT=json
 START_JSON_FILE=private.op-ratelimit-start.json
 END_JSON_FILE=private.op-ratelimit-start.json
 
-echo GITHUB_JOB=$GITHUB_JOB
-echo GITHUB_RUN_ID=$GITHUB_RUN_ID
-
 [ -z "${GITHUB_JOB}" ] && echo FATAL GITHUB_JOB is not set # && exit 1
 [ -z "${GITHUB_RUN_ID}" ] && echo FATAL GITHUB_RUN_ID is not set # && exit 1
 
-# op service-account ratelimit > 
+echo GITHUB_JOB=$GITHUB_JOB
+echo GITHUB_RUN_ID=$GITHUB_RUN_ID
+DATETIME_STAMP=$(date -I)
+PREFIX=${DATETIME_STAMP}_${GITHUB_JOB}_${GITHUB_RUN_ID}
+START_JSON=${PREFIX}-start.json
+END_JSON=${PREFIX}-end.json
+
+if [ ! -e ${START_JSON} ]; then 
+	op service-account ratelimit | tee $START_JSON
+else
+	op service-account ratelimit | tee $END_JSON
+fi
 
 # echo $GITHUB_CONTEXT | yq '.job'
 
