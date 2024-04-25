c2e4030d7ecc848a7579dafc121b38e1f200aea9
santan maddi <saisantan2004@gmail.com>
2024-04-25 14:59:32 +0800

    successful alp-fhir-fe

--- services/alp-fhir/ui-file-server/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use('/', express.static('ui-files/dist'))
-
-app.listen(port, () => {
-  console.log(`Local UI File Server listening on port ${port}`)
-})
