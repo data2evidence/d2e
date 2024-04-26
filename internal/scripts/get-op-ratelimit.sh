72546c2409e98f8c28e381dd352fd66bc415c256
Brian Cornally <brian.cornally@data4life-asia.care>
2024-04-26 15:32:11 +0800

    test

--- internal/scripts/get-op-ratelimit.sh
+++ internal/scripts/get-op-ratelimit.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 # [ -z "${GITHUB_JOB}" ] && echo FATAL GITHUB_JOB is not set # && exit 1
 # [ -z "${GITHUB_RUN_ID}" ] && echo FATAL GITHUB_RUN_ID is not set # && exit 1
-export OP_FORMAT=json
-export OP_FORMAT=human-readable
+# export OP_FORMAT=json
+# export OP_FORMAT=human-readable
 
 if [ -z "${GITHUB_JOB}" ]; then 
 	PREFIX=private
@@ -13,23 +13,26 @@ else
 	DATETIME_STAMP=$(date -I)
 	PREFIX=${GITHUB_JOB}_${GITHUB_RUN_ID}
 fi
-START_JSON=${PREFIX}-start.json
-END_JSON=${PREFIX}-end.json
+START_YML=${PREFIX}-start.yml
+END_YML=${PREFIX}-end.yml
+STATS_YML=${PREFIX}-stats.yml
+touch $STATS_YML
 
 op service-account ratelimit 
 
-if [ ! -e ${START_JSON} ]; then 
-	echo . set $START_JSON
-	op --format json service-account ratelimit | tee $START_JSON | yq -o tsv
+if [ ! -e ${START_YML} ]; then 
+	echo . set $START_YML
+	op --format json service-account ratelimit | yq -P | tee $START_YML
 else
-	echo . set $END_JSON
-	op --format json service-account ratelimit | tee $END_JSON | yq -o tsv
-	export START_JSON
-	export END_JSON
-	yq --null-input '"token_rd_delta=" + load(env(END_JSON)).[1].used - load(env(START_JSON)).[1].used' 
-	yq --null-input '"token_reset_mins=" + (load(env(END_JSON)).[1].reset / 60)' 
-	yq --null-input '"account_rw_delta=" + load(env(END_JSON)).[2].used - load(env(START_JSON)).[2].used' 
-	yq --null-input '"account_reset_mins=" + (load(env(END_JSON)).[2].reset / 60)' 
+	echo . set $END_YML
+	op --format json service-account ratelimit | yq -P | tee $END_YML
+	export START_YML END_YML
+	yq -i '.token_rd_delta = load(env(END_YML)).[1].used - load(env(START_YML)).[1].used' $STATS_YML
+	yq -i '.token_reset_mins = (load(env(END_YML)).[1].reset / 60)' $STATS_YML
+	yq -i '.account_rw_delta = load(env(END_YML)).[2].used - load(env(START_YML)).[2].used' $STATS_YML
+	yq -i '.account_reset_mins = (load(env(END_YML)).[2].reset / 60)' $STATS_YML
+	cat $STATS_YML
+	FILE=$STATS_YML
 fi
 
 # echo $GITHUB_CONTEXT | yq '.job'
