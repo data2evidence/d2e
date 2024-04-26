33cf45fce6e3a2da39ed1d9a5bc08fe50816c795
Santan Maddi <55120902+SantanM@users.noreply.github.com>
2024-04-26 12:23:33 +0800

    log errors in hooks

--- services/alp-dataflow-gen/pysrc/flows/alp_data_characterization/hooks.py
+++ services/alp-dataflow-gen/pysrc/flows/alp_data_characterization/hooks.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from prefect.logging.loggers import flow_run_logger, task_run_logger
 from prefect.server.schemas.states import StateType
 from utils.databaseConnectionUtils import insert_to_dqd_result_table
 from utils.types import dcOptionsType
@@ -8,6 +9,7 @@ import importlib
 
 
 async def drop_data_characterization_schema(flow, flow_run, state):
+    logger = flow_run_logger(flow_run, flow)
     options = dcOptionsType(**flow_run.parameters['options'])
     databaseCode = options.databaseCode
     resultsSchema = options.resultsSchema
@@ -18,10 +20,12 @@ async def drop_data_characterization_schema(flow, flow_run, state):
         dbsvc_module = importlib.import_module('d2e_dbsvc')
         await dbsvc_module._run_db_svc_shell_command("delete", request_url, {})
     except Exception as e:
+        logger.error(e)
         raise e
 
 
 async def persist_data_characterization(task, task_run, state, output_folder):
+    logger = task_run_logger(task_run, task)
     error_message = None
     result_json = {}
     is_error = state.type == StateType.FAILED
@@ -29,6 +33,7 @@ async def persist_data_characterization(task, task_run, state, output_folder):
         is_error = True
         with open(f'{output_folder}/errorReportR.txt', 'rt') as f:
             error_message = f.read()
+        logger.error(error_message)
     else:
         # Data_characterization run does not need to insert data into table on completion
         return
@@ -41,11 +46,13 @@ async def persist_data_characterization(task, task_run, state, output_folder):
 
 
 async def persist_export_to_ares(task, task_run, state, output_folder, schema_name):
+    logger = task_run_logger(task_run, task)
     error_message = None
     result_json = {}
     is_error = state.type == StateType.FAILED
     if is_error:
         error_message = await get_export_to_ares_execute_error_message_from_file(output_folder, schema_name)
+        logger.error(error_message)
     else:
         result_json = await get_export_to_ares_results_from_file(output_folder, schema_name)
     await insert_to_dqd_result_table(flow_run_id=task_run.flow_run_id, result=result_json, error=is_error, error_message=error_message)
