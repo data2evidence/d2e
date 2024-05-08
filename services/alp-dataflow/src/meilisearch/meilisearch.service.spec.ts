a426827533e01e52ab9c1ef9b9de8c5e2594a314
Brandan <56997231+brandantck@users.noreply.github.com>
2024-05-08 12:45:25 +0800

    [DP-452] Meilisearch index flow use client credential token (#72)
    * add meilisearch client credentials scopes to gateway
    
    * use client cred token and remove token as input param
    
    * remove token as input param to meilisearch index flow
    
    * add pyjwt
    
    * update to use alp-data client credentials
    
    * verify jwt instead of manually calculating expiry

--- services/alp-dataflow/src/meilisearch/meilisearch.service.spec.ts
+++ services/alp-dataflow/src/meilisearch/meilisearch.service.spec.ts
@@ -1,4 +1,3 @@
-import { REQUEST } from '@nestjs/core'
 import { Test, TestingModule } from '@nestjs/testing'
 import { MeilisearchService } from './meilisearch.service'
 import { PrefectAPI } from '../prefect/prefect.api'
@@ -8,17 +7,8 @@ describe('MeilisearchService', () => {
   let service: MeilisearchService
 
   beforeEach(async () => {
-    const req = {
-      headers: {
-        authorization: 'Bearer token'
-      }
-    }
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        MeilisearchService,
-        { provide: REQUEST, useValue: req },
-        { provide: PrefectAPI, useFactory: prefectApiMockFactory }
-      ]
+      providers: [MeilisearchService, { provide: PrefectAPI, useFactory: prefectApiMockFactory }]
     }).compile()
 
     service = await module.resolve<MeilisearchService>(MeilisearchService)
