82fd6c3b33fd3b16491d4ffa94ab7c6a26882d46
alicia-koh <alicia.koh@data4life-asia.care>
2024-05-08 10:19:33 +0800

    Update hooks.py

--- services/alp-dataflow-gen/pysrc/flows/alp_data_characterization/hooks.py
+++ services/alp-dataflow-gen/pysrc/flows/alp_data_characterization/hooks.py
@@ -9,17 +9,17 @@ import importlib
 from flows.alp_db_svc.flow import _run_db_svc_shell_command, _get_db_dialect
 
 
-async def drop_data_characterization_schema(flow, flow_run, state, options: dcOptionsType):
+async def drop_data_characterization_schema(flow, flow_run, state, dropSchemaOptions: dcOptionsType):
     logger = flow_run_logger(flow_run, flow)
     options = dcOptionsType(**flow_run.parameters['options'])
-    database_code = options.databaseCode
-    results_schema = options.resultsSchema
+    database_code = dropSchemaOptions.databaseCode
+    results_schema = dropSchemaOptions.resultsSchema
 
     request_type = requestType.DELETE
     request_body = {}
 
     try:
-        db_dialect = _get_db_dialect(options)
+        db_dialect = _get_db_dialect(dropSchemaOptions)
         request_url = f"/alpdb/{db_dialect}/dataCharacterization/database/{database_code}/schema/{results_schema}"
         await _run_db_svc_shell_command(request_type, request_url, request_body)
     except Exception as e:
