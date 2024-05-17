ebdf60a0f356fd69a24bd75d51453485e33649dd
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2024-05-17 19:10:12 +0800

    Transform meilisearch to plugin (#61)

--- services/alp-dataflow-gen/init.sh
+++ services/alp-dataflow-gen/init.sh
@@ -2,6 +2,5 @@
 python ./init.py
 prefect deployment apply ./pysrc/flows/alp_db_svc/execute_alp_db_svc_flow-deployment.yaml
 prefect deployment apply ./pysrc/flows/alp_db_svc/execute_seed_postgres_data_flow-deployment.yaml
-prefect deployment apply ./pysrc/flows/meilisearch/execute_meilisearch_add_index_flow-deployment.yaml
 prefect deployment apply ./pysrc/flows/portal_server/update_dataset_attributes_flow-deployment.yaml
 prefect deployment apply ./pysrc/flows/strategus/execute_strategus-deployment.yaml
