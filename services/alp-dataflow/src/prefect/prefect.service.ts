160adcdea17738f5e185930a8ad23d1559eb6232
Khairul Syazwan <khairul.masri@data4life-asia.care>
2024-06-07 08:34:23 +0800

    Add missing import

--- services/alp-dataflow/src/prefect/prefect.service.ts
+++ services/alp-dataflow/src/prefect/prefect.service.ts
@@ -13,6 +13,7 @@ import {
   IPrefectFlowRunByMetadataDto
 } from '../types'
 import { PrefectExecutionClient } from './prefect-execution.client'
+import { env } from '../env'
 import { createLogger } from '../logger'
 import {
   FLOW_METADATA,
