e19fce46b11bf127bc84e726bda2c47ab6ef15eb
Jerome Ng <jerome.ng@data4life-asia.care>
2024-06-13 23:09:13 +0800

    minor refactor

--- services/alp-dataflow-gen/pysrc/flows/cohort_survival/jerome.R
+++ services/alp-dataflow-gen/pysrc/flows/cohort_survival/jerome.R
@@ -11,7 +11,7 @@ library(tools)
 
 # VARIABLES
 filename <- "alpdev_pg_cdmdefault"
-exposure_cohort_definition_id <- 3
+target_cohort_definition_id <- 3
 outcome_cohort_definition_id <- 4
 # END VARIABLES
 duckdb_dir <- Sys.getenv("DUCKDB__DATA_FOLDER")
@@ -22,53 +22,47 @@ random_string <- function(n = 6) {
 }
 # Generate a random postfix and create a new filename
 postfix <- random_string()
-new_filename <- paste0(filename, "_", postfix)
-new_filepath <- file.path(duckdb_dir, new_filename)
+duplicate_filepath <- file.path(duckdb_dir, paste0(filename, "_", postfix))
 
 # Copy the original file to the new file
-file.copy(filepath, new_filepath)
+file.copy(filepath, duplicate_filepath)
+print(paste("Created duckdb File: ", duplicate_filepath))
 con <- NULL
 tryCatch(
     {
-        duckdb_con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
-        postgres_con_query <- "INSTALL postgres_scanner;LOAD postgres_scanner;CREATE TABLE jtable AS FROM (SELECT * FROM postgres_scan('host=alp-minerva-postgres-1 port=5432 dbname=alpdev_pg user=postgres_tenant_read_user password=Toor1234', 'cdmdefault',  'cohort'))"
-        result <- DBI::dbExecute(duckdb_con, postgres_con_query)
-        exposure_cohort <- DBI::dbGetQuery(duckdb_con, sprintf("SELECT * from jtable WHERE cohort_definition_id = %d;", exposure_cohort_definition_id))
-        outcome_cohort <- DBI::dbGetQuery(duckdb_con, sprintf("SELECT * from jtable WHERE cohort_definition_id = %d;", outcome_cohort_definition_id))
+        duckdb_pg_con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
+        postgres_con_query <- "INSTALL postgres_scanner;LOAD postgres_scanner;CREATE TABLE duckdb_cohort AS FROM (SELECT * FROM postgres_scan('host=alp-minerva-postgres-1 port=5432 dbname=alpdev_pg user=postgres_tenant_read_user password=Toor1234', 'cdmdefault',  'cohort'))"
+        result <- DBI::dbExecute(duckdb_pg_con, postgres_con_query)
+        target_cohort <- DBI::dbGetQuery(duckdb_pg_con, sprintf("SELECT * from duckdb_cohort WHERE cohort_definition_id = %d;", target_cohort_definition_id))
+        outcome_cohort <- DBI::dbGetQuery(duckdb_pg_con, sprintf("SELECT * from duckdb_cohort WHERE cohort_definition_id = %d;", outcome_cohort_definition_id))
 
-        duckdb_con <- DBI::dbConnect(duckdb::duckdb(dbdir = new_filepath, read_only = FALSE))
+        duckdb_con <- DBI::dbConnect(duckdb::duckdb(dbdir = duplicate_filepath, read_only = FALSE))
 
         # Write the data frame to DuckDB as a table
-        DBI::dbWriteTable(duckdb_con, "exposure_cohort", exposure_cohort)
+        DBI::dbWriteTable(duckdb_con, "target_cohort", target_cohort)
         DBI::dbWriteTable(duckdb_con, "outcome_cohort", outcome_cohort)
 
-        # These should be untouched from db i.e. overwrite = FALSE
-        # DBI::dbWriteTable(duckdb_con, "observation_period", observation_period, overwrite = TRUE)
-        # DBI::dbWriteTable(duckdb_con, "person", person, overwrite = TRUE)
-
         # cdm_from_con is from CDMConnection
+        # duckdb uses 'main' as default schema name
         cdm <- cdm_from_con(
             con = duckdb_con,
-            cdm_schema = "main",
             write_schema = "main",
-            cdm_name = "alpdev_pg_cdmdefault",
-            cohort_tables = c("exposure_cohort", "outcome_cohort"),
+            cdm_schema = "main",
+            cohort_tables = c("target_cohort", "outcome_cohort"),
             .soft_validation = TRUE
         )
 
         death_survival <- estimateSingleEventSurvival(cdm,
-            targetCohortTable = "exposure_cohort",
+            targetCohortTable = "target_cohort",
             outcomeCohortTable = "outcome_cohort"
         )
-        death_survival |>
-            glimpse()
 
         plot <- plotSurvival(death_survival)
         plot_data <- ggplot_build(plot)$data[[1]]
         my_json <- toJSON(plot_data)
         print(my_json)
         # TODO: Send it to save
-        print(paste("Created duckdb File: ", new_filepath))
+        cdm_disconnect(cdm)
     },
     error = function(e) {
         print(e)
@@ -78,7 +72,7 @@ tryCatch(
             DBI::dbDisconnect(con)
             print("Disconnected the database.")
         }
-        file.remove(new_filepath)
+        file.remove(duplicate_filepath)
         print("Removed temporary duckdb file")
     }
 )
