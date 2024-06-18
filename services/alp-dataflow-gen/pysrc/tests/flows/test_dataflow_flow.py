f7d6245d936687b0923a1a381cec681d58dd0f4c
santan maddi <saisantan2004@gmail.com>
2024-06-18 15:06:09 +0800

    revert

--- services/alp-dataflow-gen/pysrc/tests/flows/test_dataflow_flow.py
+++ services/alp-dataflow-gen/pysrc/tests/flows/test_dataflow_flow.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 from main import execute_dataflow_flow
 import alpconnection.dbutils as dbutils
@@ -8,7 +7,6 @@ import nodes.nodes as nodes
 import dask.dataframe as dd
 import pandas as pd
 from utils.types import PG_TENANT_USERS
-from prefect.testing.utilities import prefect_test_harness
 
 
 @pytest.fixture
@@ -28,15 +26,9 @@ def mock_GetDBConnection(db_name, user_type):
 def mock_persist_results_flow(nodes, trace, root_flow_run_id):
     return "_dummy_persist_results_flow"
 
-@pytest.fixture(autouse=True, scope="session")
-def prefect_test_fixture():
-    with prefect_test_harness():
-        yield
-
 def test_execute_dataflow_flow(setup, mock_dataflow):
     # results object will have 2 items, [generate_nodes_flow, execute_nodes_flow].
     # persist_results_flow is not in results as it is currently being mocked
-    os.environ.setdefault('PREFECT_RESULTS_PERSIST_BY_DEFAULT', False)
     results = execute_dataflow_flow(
         mock_dataflow["json_graph"], mock_dataflow["options"])
 
