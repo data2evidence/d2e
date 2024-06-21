1541c65c8d5a14c86476022c93321525a05dc694
Khairul Syazwan <khairul.masri@data4life-asia.care>
2024-06-21 23:04:47 +0800

    Add seeds

---
+++ services/alp-portal-server/src/common/data-source/seeds/overview-description.seeder.ts
@@ -0,0 +1,22 @@
+import { Seeder } from 'typeorm-extension'
+import { DataSource } from 'typeorm'
+import { OverviewDescription } from '../../../overview-description/entity'
+
+export default class OverviewDescriptionSeeder implements Seeder {
+  public async run(dataSource: DataSource): Promise<void> {
+    const repository = dataSource.getRepository(OverviewDescription)
+
+    const result = await repository.createQueryBuilder('overview_description').getMany()
+    if (result.length > 0) {
+      return
+    }
+
+    const entities = repository.create({
+      id: '737bcc1c-c39d-48d7-a333-0929ae1020e7',
+      text: 'Our vision is a world where health data is comprehensively, digitally, and securely available for research and directly impacts the prevention, diagnosis, and treatment of diseases.',
+      createdBy: 'system',
+      modifiedBy: 'system'
+    })
+    await repository.save(entities)
+  }
+}
