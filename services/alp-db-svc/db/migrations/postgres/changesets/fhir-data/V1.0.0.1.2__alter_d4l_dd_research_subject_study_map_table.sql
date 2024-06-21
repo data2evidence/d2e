01706e64c92e10273f7673e471247dc2a186ee01
alicia-koh <alicia.koh@data4life-asia.care>
2024-06-21 11:48:02 +0800

    Remove db-svc-mgmt

--- services/alp-db-svc/db/migrations/postgres/changesets/fhir-data/V1.0.0.1.2__alter_d4l_dd_research_subject_study_map_table.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "d4l_dd_research_subject_study_map" 
-RENAME COLUMN id TO person_id;
-
-ALTER TABLE "d4l_dd_research_subject_study_map"
-RENAME TO d4l_dd_alp_id_metadata;
-
-ALTER SEQUENCE "d4l_dd_research_subject_study_map_id_seq"
-RENAME TO d4l_dd_alp_id_metadata_person_id_seq;
--- services/alp-db-svc/db/migrations/postgres/changesets/fhir-data/V1.0.0.2.2__alter_d4l_dd_studies.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE d4l_dd_studies ALTER COLUMN "processed" DROP DEFAULT;
-ALTER TABLE d4l_dd_studies ALTER COLUMN "processed" TYPE INT USING CASE WHEN TRUE THEN 2 ELSE 0 END;
-ALTER TABLE d4l_dd_studies ALTER COLUMN "processed" SET DEFAULT 0;
-
-ALTER TABLE d4l_dd_studies ADD COLUMN "processingStatus" text;
-ALTER TABLE d4l_dd_studies ADD COLUMN "processingStartedAt" timestamp with time zone;
