88671c8933fce43b415a17c406728d3e01452fff
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2024-07-08 13:08:25 +0800

    Merge fhir-server and client services

--- services/alp-fhir-client/src/utils/types.ts
+++
@@ -1,8 +0,0 @@
-export interface MedplumBotConfig {
-    readonly name: string;
-    readonly id: string;
-    readonly description: string;
-    readonly source: string;
-    readonly dist?: string;
-    readonly subscriptionCriteria?: string;
-}
