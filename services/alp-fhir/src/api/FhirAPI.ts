ca3e327b864cf9aabd221abc4e02f454c728036f
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2024-07-08 13:07:37 +0800

    Create subscription

---
+++ services/alp-fhir/src/api/FhirAPI.ts
@@ -0,0 +1,109 @@
+
+import { CreateBinaryOptions, MedplumClient, OperationOutcomeError } from '@medplum/core'
+import { Resource, Attachment, Bot, Subscription } from '@medplum/fhirtypes'
+import { env } from '../env'
+import { createLogger } from '../logger'
+
+export class FhirAPI {
+    private readonly clientId: string
+    private readonly clientSecret: string
+    private readonly logger = createLogger(this.constructor.name)
+    private medplumClient: MedplumClient
+
+    constructor() {
+        if (env.FHIR_CLIENT_ID && env.FHIR_CLIENT_SECRET) {
+            this.clientId = env.FHIR_CLIENT_ID
+            this.clientSecret = env.FHIR_CLIENT_SECRET
+        } else {
+            this.logger.error('No client credentials are set for Fhir')
+            throw new Error('No client credentials are set for Fhir')
+        }
+        this.medplumClient = new MedplumClient({
+            baseUrl: env.SERVICE_ROUTES.fhir
+        })
+    }
+
+    async clientCredentialslogin() {
+        try {
+            return await this.medplumClient.startClientLogin(this.clientId, this.clientSecret)
+        } catch (error) {
+            console.log(JSON.stringify(error))
+            this.logger.error('Error performing client credentials authentication', error)
+        }
+    }
+
+    // async createResource_Project(name: string, description: string) {
+    //     try {
+    //         return await this.medplumClient.createResource<Project>({
+    //             resourceType: 'Project',
+    //             name: name,
+    //             description: description,
+    //             features: ['bots']
+    //         })
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
+    async readResource_bot(id: string){
+        try{
+            return await this.medplumClient.readResource('Bot', id);
+        }catch(err){
+            if(err instanceof OperationOutcomeError && err.outcome.id == 'not-found'){
+                let bot: Bot = {
+                    resourceType: 'Bot'
+                }
+                return bot
+            }else{
+                console.log("Not of type Operation outcome")
+                throw err;
+            }
+        }
+    }
+
+    async searchResource_bot(query: string){
+        return await this.medplumClient.searchOne('Bot', query=query)
+    }
+
+    async create_bot(url: string, body: any){
+       return await this.medplumClient.post(url, body);
+    }
+
+    async createAttachment_bot(fileName: string, fileContent: string, fileContentType: string){
+        let binaryOptions: CreateBinaryOptions = {
+            filename: fileName,
+            data: fileContent,
+            contentType: fileContentType
+        }
+        return await this.medplumClient.createAttachment(binaryOptions)
+    }
+
+    async updateResource_bot(resource: Resource, sourceCode: Attachment){
+        return await this.medplumClient.updateResource({
+            ...resource,
+            sourceCode
+        })
+    }
+
+    fhirUrl_bot(action: string, botId?: string){
+        return this.medplumClient.fhirUrl('Bot', botId as string, action)
+    }
+
+    // async executeBot(botId: string){
+    //     return await this.medplumClient.executeBot(botId, {})
+    // }
+
+    async createResource_subscription(endpoint: string, reason: string, criteria: any){
+        return await this.medplumClient.createResource<Subscription>({
+            resourceType: 'Subscription',
+            status: 'active',
+            reason: reason,
+            channel: { type: 'rest-hook', endpoint:  endpoint},
+            criteria: criteria
+        });
+    }
+    
+    async searchResource_subscription(query: string){
+        return await this.medplumClient.search('Subscription', query=query)
+    }
+}
---
+++ services/alp-fhir/src/utils/botUtils.ts
@@ -0,0 +1,123 @@
+import { MedplumBotConfig } from "./types";
+import { Bot, OperationOutcome } from '@medplum/fhirtypes'
+import * as botConfig from '../bots.config.json'
+import { FhirAPI } from "../api/FhirAPI";
+import { basename, resolve } from "path";
+import { existsSync, readFileSync } from "fs";
+import { ContentType } from "@medplum/core";
+
+//Reads bots.config.json file and creates bot for each and deploys it
+export async function readAndCreateBotFromConfig(){
+    const botConfigs = botConfig.bots;
+    try{
+        if (!botConfigs) {
+            console.log('0 bots configured in bot config.')
+            return [];
+        }
+        console.log(botConfig.bots.length + ' bots configured in bot config.')
+        let saved = 0;
+        for (const botConfig of botConfigs) {
+            let fhirApi = new FhirAPI()
+            await fhirApi.clientCredentialslogin()
+            await createBot(fhirApi, 'Project Id', botConfig, 'vmcontext');
+            saved++;
+            console.log(`Bot ${botConfig.name} saved and deployed successfully!`)
+        }
+        console.log('All bots are saved and deployed successfully!')
+    } catch (err: unknown) {
+        console.log(JSON.stringify(err))
+        console.log(`Failed to save bots`)
+    }
+}
+
+async function createBot(
+    fhirApi: FhirAPI,
+    projectId: string,
+    botConfig: MedplumBotConfig,
+    runtimeVersion: string,
+  ): Promise<void> {
+    const body = {
+      name: botConfig.name,
+      description: botConfig.description,
+      runtimeVersion:runtimeVersion
+    };
+    try{
+        //Check if the bot exists
+        console.log(`Check if bot ${botConfig.id} already exists in DB`)
+        let bot:Bot
+        let searchResult = await fhirApi.searchResource_bot('name='+botConfig.name);
+        if(searchResult){
+            bot = searchResult; 
+        }else{
+            console.log(`Create new bot ${botConfig.name}`)     
+            let newBot = await fhirApi.create_bot('admin/projects/' + projectId + '/bot', body)
+            bot = await fhirApi.readResource_bot(newBot.id);
+        }
+        await saveBot(fhirApi, botConfig as MedplumBotConfig, bot);
+        await deployBot(fhirApi, botConfig as MedplumBotConfig, bot);
+        await createSubscription(fhirApi, botConfig as MedplumBotConfig, bot)
+        console.log(`Success! Bot created: ${bot.id}`);
+    }catch(err){
+        console.log('Failed to create bot: ' + botConfig.name);
+        throw err;
+    }
+}
+
+async function saveBot(fhirApi: FhirAPI, botConfig: MedplumBotConfig, bot: Bot): Promise<void> {
+    try{
+        const codePath = botConfig.source;
+        const code = readFileContents(codePath);
+        if (!code) {
+          return;
+        }
+        console.log('Saving source code...');
+        const sourceCode = await fhirApi.createAttachment_bot(basename(codePath), code, ContentType.TYPESCRIPT);
+      
+        console.log('Updating bot...');
+        const updateResult = await fhirApi.updateResource_bot(bot, sourceCode);
+        console.log('Success! New bot version: ' + updateResult.meta?.versionId);
+    }catch(err){
+        console.log('Failed to save bot: ' + botConfig.name);
+        throw err;
+    }
+}
+
+async function deployBot(fhirApi: FhirAPI, botConfig: MedplumBotConfig, bot: Bot): Promise<void> {
+    const codePath = botConfig.dist ?? botConfig.source;
+    try{
+        const code = readFileContents(codePath);
+        if (!code) return;
+        console.log('Deploying bot...');
+        console.log(fhirApi.fhirUrl_bot('$deploy', bot.id).toString())
+        const deployResult = (await fhirApi.create_bot(
+            fhirApi.fhirUrl_bot('$deploy', bot.id).toString(), 
+            {
+                code,
+                filename: basename(codePath)
+            }
+        )) as OperationOutcome;
+        console.log('Deploy result: ' + deployResult.issue?.[0]?.details?.text);
+    }catch(err){
+        console.log(JSON.stringify(err))
+        console.log('Failed to deploy bot: ' + botConfig.name);
+        throw err;
+    }
+}
+
+async function createSubscription(fhirApi: FhirAPI, botConfig: MedplumBotConfig, bot: Bot){
+    const result = await fhirApi.searchResource_subscription(`criteria=${botConfig.subscriptionCriteria}`)
+    const botSubscription = result.entry?.filter((x) => x.resource?.channel.endpoint == 'Bot/' + bot.id)
+    if (botSubscription?.length == 0) {
+        // Create new subscription
+        await fhirApi.createResource_subscription(`Bot/${bot.id}`, `Rest hook subscription for ${botConfig.subscriptionCriteria} resource`, botConfig.subscriptionCriteria);
+        console.log(`Subscription for bot: ${bot.name} successfully created!`)
+    }
+}
+
+function readFileContents(fileName: string): string {
+    const path = resolve(fileName);
+    if (!existsSync(path)) {
+        return '';
+    }
+    return readFileSync(path, 'utf8');
+}
