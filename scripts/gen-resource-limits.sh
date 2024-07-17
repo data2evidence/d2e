3ff968d8ea777b2fe552d8df7931995b60136fde
Brandan <brandan.tanck@gmail.com>
2024-07-17 15:40:33 +0800

    add script to calculate resource limits

---
+++ scripts/gen-resource-limits.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# inputs
+D2E_RESOURCE_PERCENTAGE=${D2E_RESOURCE_PERCENTAGE:-0.7}
+ENV_TYPE=${ENV_TYPE:-local}
+
+# vars
+DOTENV_FILE=.env.$ENV_TYPE
+touch ${DOTENV_FILE}
+
+echo D2E_RESOURCE_PERCENTAGE=$D2E_RESOURCE_PERCENTAGE
+
+get_cpu_count() {
+    OS="$(uname -s)"
+    if [ "$OS" = "Linux" ]; then
+        NPROCS="$(nproc --all)"
+    elif [ "$OS" = "Darwin" ] || \
+            [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+        NPROCS="$(sysctl -n hw.ncpu)"
+    else
+        NPROCS="$(getconf _NPROCESSORS_ONLN)"  # glibc/coreutils fallback
+    fi
+    D2E_CPU_LIMIT=$(echo "scale=2;$NPROCS*$D2E_RESOURCE_PERCENTAGE" |bc)
+    # Strip decimal numbers
+    D2E_CPU_LIMIT=${D2E_CPU_LIMIT%%.*}
+
+    echo D2E_CPU_LIMIT=$D2E_CPU_LIMIT
+
+    # remove existing env var from dotenv
+    sed -i.bak "/D2E_CPU_LIMIT=/,//d" $DOTENV_FILE
+    # set env var
+    echo D2E_CPU_LIMIT=\'"$D2E_CPU_LIMIT"\' >> $DOTENV_FILE
+}
+
+get_memory() {
+    MEMORY=$(free -g | grep Mem: | awk '{print $2}')
+    D2E_MEMORY_LIMIT=$(echo "scale=2;$MEMORY*$D2E_RESOURCE_PERCENTAGE" |bc)
+    # Strip decimal numbers
+    D2E_MEMORY_LIMIT=${D2E_MEMORY_LIMIT%%.*}
+    # Add G suffix for gigabyte
+    D2E_MEMORY_LIMIT=${D2E_MEMORY_LIMIT}G
+
+    echo D2E_MEMORY_LIMIT=$D2E_MEMORY_LIMIT
+
+    # remove existing env var from dotenv
+    sed -i.bak "/D2E_MEMORY_LIMIT=/,//d" $DOTENV_FILE
+    # set env var
+    echo D2E_MEMORY_LIMIT=\'"$D2E_MEMORY_LIMIT"\' >> $DOTENV_FILE
+
+}
+
+get_cpu_count
+get_memory
