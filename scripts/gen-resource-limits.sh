be2ab4663266c8b30df0fdae281a58b0a734037d
Brandan <56997231+brandantck@users.noreply.github.com>
2024-07-18 17:00:27 +0800

    [ANALYZE-773] Set resource limits for analytics svc (#242)
    * add script to calculate resource limits
    
    * update to work for macos
    
    * rename percentage to limit
    
    * add env-file to command
    
    * add default values to env instead

---
+++ scripts/gen-resource-limits.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# inputs
+D2E_RESOURCE_LIMIT=${D2E_RESOURCE_LIMIT:-0.7}
+ENV_TYPE=${ENV_TYPE:-local}
+
+# vars
+OS="$(uname -s)"
+DOTENV_FILE=.env.$ENV_TYPE
+touch ${DOTENV_FILE}
+
+echo D2E_RESOURCE_LIMIT=$D2E_RESOURCE_LIMIT
+
+get_cpu_count() {
+    if [ "$OS" = "Linux" ]; then
+        NPROCS="$(nproc --all)"
+    elif [ "$OS" = "Darwin" ] || \
+            [ "$(echo "$OS" | grep -q BSD)" = "BSD" ]; then
+        NPROCS="$(sysctl -n hw.ncpu)"
+    else
+        NPROCS="$(getconf _NPROCESSORS_ONLN)"  # glibc/coreutils fallback
+    fi
+    D2E_CPU_LIMIT=$(echo "scale=2;$NPROCS*$D2E_RESOURCE_LIMIT" |bc)
+    # Strip decimal numbers
+    D2E_CPU_LIMIT=${D2E_CPU_LIMIT%%.*}
+    echo D2E_CPU_LIMIT=$D2E_CPU_LIMIT
+
+    # remove existing env var from dotenv
+    sed -i.bak "/D2E_CPU_LIMIT=/,//d" $DOTENV_FILE
+    # set env var
+    echo D2E_CPU_LIMIT=\'"$D2E_CPU_LIMIT"\' >> $DOTENV_FILE
+}
+
+get_memory() {
+    if [ "$OS" = "Darwin" ]; then
+        # mem_size=$(sysctl -n hw.memsize)
+        MEMORY=$(system_profiler SPHardwareDataType | grep "  Memory:" | awk '{print $2}')
+    else
+        MEMORY=$(free -g | grep Mem: | awk '{print $2}')
+    fi
+    D2E_MEMORY_LIMIT=$(echo "scale=2;$MEMORY*$D2E_RESOURCE_LIMIT" |bc)
+    # Strip decimal numbers
+    D2E_MEMORY_LIMIT=${D2E_MEMORY_LIMIT%%.*}
+    # Add G suffix for gigabyte
+    D2E_MEMORY_LIMIT=${D2E_MEMORY_LIMIT}G
+    echo D2E_MEMORY_LIMIT=$D2E_MEMORY_LIMIT
+
+    # remove existing env var from dotenv
+    sed -i.bak "/D2E_MEMORY_LIMIT=/,//d" $DOTENV_FILE
+    # set env var
+    echo D2E_MEMORY_LIMIT=\'"$D2E_MEMORY_LIMIT"\' >> $DOTENV_FILE
+
+}
+
+get_cpu_count
+get_memory
