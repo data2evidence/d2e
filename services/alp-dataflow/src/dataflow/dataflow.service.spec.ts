0c7895c5e70aae7cec8d166ef32f2d01c9f8fcee
hengxian-jiang <hengxian.jiang@u.nus.edu>
2024-07-25 10:32:39 +0800

    Fix dataflow-mgmt unit test

--- services/alp-dataflow/src/dataflow/dataflow.service.spec.ts
+++ services/alp-dataflow/src/dataflow/dataflow.service.spec.ts
@@ -4,6 +4,12 @@ import { getRepositoryToken } from '@nestjs/typeorm'
 import { DataflowService } from './dataflow.service'
 import { Dataflow, DataflowRevision, DataflowResult, DataflowRun } from './entity'
 import { repositoryMockFactory } from '../../test/repository.mock'
+import { portalApiMockFactory } from '../portal-server/portal-server.mock'
+import { PortalServerAPI } from '../portal-server/portal-server.api'
+import { PrefectAPI } from '../prefect/prefect.api'
+import { prefectApiMockFactory } from '../prefect/prefect.mock'
+import { UtilsService } from '../utils/utils.service'
+import { utilsSvcMockFactory } from '../utils/utils.mock'
 
 jest.mock('jsonwebtoken', () => ({
   decode: jest.fn().mockReturnValue({ sub: 'mock-sub' })
@@ -25,7 +31,10 @@ describe('DataflowService', () => {
         { provide: getRepositoryToken(Dataflow), useFactory: repositoryMockFactory },
         { provide: getRepositoryToken(DataflowRevision), useFactory: repositoryMockFactory },
         { provide: getRepositoryToken(DataflowRun), useFactory: repositoryMockFactory },
-        { provide: getRepositoryToken(DataflowResult), useFactory: repositoryMockFactory }
+        { provide: getRepositoryToken(DataflowResult), useFactory: repositoryMockFactory },
+        { provide: PortalServerAPI, useFactory: portalApiMockFactory },
+        { provide: PrefectAPI, useFactory: prefectApiMockFactory },
+        { provide: UtilsService, useFactory: utilsSvcMockFactory }
       ]
     }).compile()
 
--- services/alp-dataflow/src/dqd/dqd.service.spec.ts
+++ services/alp-dataflow/src/dqd/dqd.service.spec.ts
@@ -5,6 +5,10 @@ import { DqdResult } from './entity'
 import { repositoryMockFactory } from '../../test/repository.mock'
 import { PrefectAPI } from '../prefect/prefect.api'
 import { prefectApiMockFactory } from '../prefect/prefect.mock'
+import { portalApiMockFactory } from '../portal-server/portal-server.mock'
+import { PortalServerAPI } from '../portal-server/portal-server.api'
+import { UtilsService } from '../utils/utils.service'
+import { utilsSvcMockFactory } from '../utils/utils.mock'
 
 describe('DqdService', () => {
   let service: DqdService
@@ -14,7 +18,9 @@ describe('DqdService', () => {
       providers: [
         DqdService,
         { provide: PrefectAPI, useFactory: prefectApiMockFactory },
-        { provide: getRepositoryToken(DqdResult), useFactory: repositoryMockFactory }
+        { provide: getRepositoryToken(DqdResult), useFactory: repositoryMockFactory },
+        { provide: PortalServerAPI, useFactory: portalApiMockFactory },
+        { provide: UtilsService, useFactory: utilsSvcMockFactory }
       ]
     }).compile()
 
