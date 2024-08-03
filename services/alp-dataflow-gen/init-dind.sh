11bda79a2378570b55ef08b87936a7583707b265
santan maddi <saisantan2004@gmail.com>
2024-08-03 18:46:54 +0800

    fix issue with init-dnd script

--- services/alp-dataflow-gen/init-dind.sh
+++ services/alp-dataflow-gen/init-dind.sh
@@ -1,6 +1,6 @@
 
-cd /usr/src/app/flow_builder/flow_builder_plugin
-python flow.py
+cd /usr/src/app/d2e-plugins/flow_builder/flow_builder_plugin
+python flow.py sourcePath=/usr/src/app/d2e-plugins/flow_builder/flow_builder_plugin entrypoint=flow.py:build_flow
 
-/bin/bash ./dockerd-entrypoint.sh
+/bin/bash /usr/local/bin/dockerd-entrypoint.sh
 prefect worker start --pool $PREFECT__WORKER_POOL_NAME --install-policy always
