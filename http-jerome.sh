be50060c465dca0d17b3c8dd8de76f8f649a6abe
Jerome Ng <jerome.ng@data4life-asia.care>
2024-08-07 15:03:17 +0800

    update

--- http-jerome.sh
+++ http-jerome.sh
@@ -86,7 +86,7 @@ docker exec -it alp-minerva-postgres-1 psql -h localhost -U postgres -p 5432 -d 
 # *** End postgres for seeding db credentials ***
 
 # Start system services
-yarn start:minerva-test alp-caddy alp-minerva-postgres alp-logto alp-minerva-gateway alp-minerva-user-mgmt alp-minerva-portal-server alp-query-gen alp-bookmark alp-minerva-pg-mgmt-init alp-db-credentials-mgr alp-minerva-analytics-svc alp-minerva-pa-config-svc alp-minerva-cdw-svc alp-minerva-s3 alp-minio-post-init -d --force-recreate
+yarn start:minerva-test alp-caddy alp-minerva-postgres alp-logto alp-minerva-gateway alp-minerva-user-mgmt alp-minerva-portal-server alp-query-gen alp-bookmark alp-minerva-pg-mgmt-init alp-db-credentials-mgr alp-minerva-analytics-svc alp-minerva-pa-config-svc alp-minerva-cdw-svc alp-minerva-s3 alp-minio-post-init -d
 sleep 120
 
 ### START OF LOG IN TO GET BEARER TOKEN
