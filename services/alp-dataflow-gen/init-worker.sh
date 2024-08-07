fdd643f9723c41b6e18b85bd2cf3efca534d8cea
santan maddi <saisantan2004@gmail.com>
2024-08-07 23:38:24 +0800

    create flow_builder deployment just once

--- services/alp-dataflow-gen/init-worker.sh
+++ services/alp-dataflow-gen/init-worker.sh
@@ -1,5 +1,9 @@
-. /usr/src/app/venv/bin/activate
-cd /usr/src/app/d2e-plugins/flow_builder/flow_builder_plugin
-python flow.py cmd=deploy
+
+# create a deployment on the process-worker container alone
+if [[ "$PREFECT__WORKER_POOL_NAME" == "process-pool" ]]; then
+    . /usr/src/app/venv/bin/activate
+    cd /usr/src/app/d2e-plugins/flow_builder/flow_builder_plugin
+    python flow.py cmd=deploy
+fi
 
 prefect worker start --pool $PREFECT__WORKER_POOL_NAME --install-policy always 
