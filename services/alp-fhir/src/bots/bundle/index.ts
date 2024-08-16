135c3e6b13e1e167f23f701a8b55b090214521f1
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2024-08-16 15:01:38 +0800

    More changes

--- services/alp-fhir/src/bots/bundle/index.ts
+++ services/alp-fhir/src/bots/bundle/index.ts
@@ -1,11 +1,13 @@
 import { BotEvent, MedplumClient } from '@medplum/core';
 import { Bundle } from '@medplum/fhirtypes';
+import { createResourceInFhir } from './insert';
 
 export async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {
   const bundle = event.input as Bundle;
-  bundle.entry?.forEach((entry) =>{
+  bundle.entry?.forEach(async (entry) =>{
     console.log('Create resource for each of the entry in the bundle')
-    return medplum.createResource(entry.resource);
+    await createResourceInFhir(entry.resource)
+    //return medplum.createResource(entry.resource);
   })
   return true;
 }
--- services/alp-fhir/src/bots/patient/index.ts
+++
@@ -1,8 +0,0 @@
-import { BotEvent, MedplumClient } from '@medplum/core';
-import { Patient } from '@medplum/fhirtypes';
-
-export async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {
-  const bundle = event.input as Patient;
- 
-  return true;
-}
