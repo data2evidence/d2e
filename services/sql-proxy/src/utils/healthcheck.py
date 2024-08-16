abe2b78bfcb9946e4ed83d402412b1b68d736d90
Satish <satish.anbazhagan@data4life-asia.care>
2024-08-16 17:42:39 +0800

    Add healthcheck and as a dependency to analytics svc

---
+++ services/sql-proxy/src/utils/healthcheck.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+
+def tcp_port_check(Host, Port):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(2)  # Timeout in case of port not open
+    try:
+        client.connect((Host, Port))
+        message = 'Is everything ok?'
+        client.sendall(message.encode())
+        client.close()
+        sys.exit(0)
+    except Exception as error:
+        print("An exception occurred:", error)
+        client.close()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1].strip())
+    print(port)
+    tcp_port_check("localhost", port)
