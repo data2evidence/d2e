5988c02529df651f755d3de717bc10c5f8d212ed
Satish <satish.anbazhagan@data4life-asia.care>
2024-08-22 12:39:44 +0800

    Pause worker and docker pool

--- services/alp-dataflow-gen/init.sh
+++ services/alp-dataflow-gen/init.sh
@@ -1,7 +1,13 @@
+prefect work-pool create default-agent-pool --type prefect-agent --set-as-default
+
 # create a docker worker pool; cmd will fail if work-pool already exists
-# prefect work-pool create $PREFECT__DOCKER_WORKER_POOL_NAME --type docker
-# prefect work-pool create $PREFECT__PROCESS_WORKER_POOL_NAME --type process
-prefect work-pool create default-agent-pool --type prefect-agent
+prefect work-pool create $PREFECT__DOCKER_WORKER_POOL_NAME --type docker
+prefect work-pool create $PREFECT__PROCESS_WORKER_POOL_NAME --type process
+
+ #TODO: Remove paused later
+prefect work-pool pause $PREFECT__DOCKER_WORKER_POOL_NAME
+prefect work-pool pause $PREFECT__PROCESS_WORKER_POOL_NAME
+
 
 # initialise prefect flow deployments
 python ./init.py
