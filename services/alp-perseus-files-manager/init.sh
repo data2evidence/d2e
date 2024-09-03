9ca4e81c4b58ba7961766b09b44a834fd36961e3
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-09-03 17:54:57 +0800

    remove unneeded psql

--- services/alp-perseus-files-manager/init.sh
+++ services/alp-perseus-files-manager/init.sh
@@ -1,13 +1,4 @@
 PGPASSWORD=${PG_SUPER_PASSWORD} psql -h ${PG__HOST} -d ${PG__DB_NAME} -U ${PG_SUPER_USER} -p ${PG__PORT} -a \
 -c "CREATE SCHEMA files_manager;" \
--c "GRANT ALL PRIVILEGES ON SCHEMA files_manager TO $PG_SUPER_USER;" 
-
-
-# echo "Generating database credentials in application-docker.yml"
-sed -i "s/{PG__HOST}/${PG__HOST}/g" ./config/application-docker.yml
-sed -i "s/{PG__PORT}/${PG__PORT}/g" ./config/application-docker.yml
-sed -i "s/{PG__DB_NAME}/${PG__DB_NAME}/g" ./config/application-docker.yml
-sed -i "s/{PG_SUPER_PASSWORD}/${PG_SUPER_PASSWORD}/g" ./config/application-docker.yml
-sed -i "s/{PG_SUPER_USER}/${PG_SUPER_USER}/g" ./config/application-docker.yml
 
 ./entrypoint.sh
--- services/alp-perseus-white-rabbit/init.sh
+++ services/alp-perseus-white-rabbit/init.sh
@@ -1,5 +1,4 @@
 PGPASSWORD=${PG_SUPER_PASSWORD} psql -h ${PG__HOST} -d ${PG__DB_NAME} -U ${PG_SUPER_USER} -p ${PG__PORT} -a \
--c "CREATE SCHEMA white_rabbit;" \
--c "GRANT ALL PRIVILEGES ON SCHEMA white_rabbit TO $PG_SUPER_USER;"
+-c "CREATE SCHEMA "white_rabbit";"
 
 java ${JAVA_OPTS} -jar /app.jar ${0} ${@}
