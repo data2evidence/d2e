59dde1bbcaeaef4aff97df1456841fe3f22a72b6
Nicholas Ang <66071629+KangJingA@users.noreply.github.com>
2024-09-11 12:49:10 +0800

    [Integrate 904] Create custom docker image for each Perseus service (#323)
    * local alp-perseus-backend
    
    * Update config.py
    
    * local alp-perseus-files-manager
    
    * local alp-perseus-cdm-builder
    
    * local alp-perseus-white-rabbit
    
    * Update application-docker.yml
    
    * Update application-docker.yml
    
    * seed to correct schema
    
    * remove unneeded psql
    
    * update datasource url
    
    * remove aliases and postgres-init
    
    * Update docker-compose.yml
    
    * remove perseushub images
    
    * use original image for white-rabbit
    
    * deploy custom perseus images
    
    * run perseus in remote
    
    * Update docker-push.yml
    
    * update files-manager container name
    
    * Update README.md
    
    * remove perseus deploy
    
    * Update docker-push.yml
    
    * hardcode vocab schema
    
    * Update README.md

---
+++ services/alp-perseus-cdm-builder/init.sh
@@ -0,0 +1,15 @@
+PGPASSWORD=${PG_SUPER_PASSWORD} psql -h ${PG__HOST} -d ${PG__DB_NAME} -U ${PG_SUPER_USER} -p ${PG__PORT} -a \
+-c "CREATE SCHEMA builder;" \
+-c "GRANT ALL PRIVILEGES ON SCHEMA builder TO $PG_SUPER_USER;" \
+
+echo "Generating database credentials in appsettings.Docker.json"
+sed -i "s/{PG__HOST}/${PG__HOST}/g" ./config/appsettings.Docker.json
+sed -i "s/{PG__PORT}/${PG__PORT}/g" ./config/appsettings.Docker.json
+sed -i "s/{PG__DB_NAME}/${PG__DB_NAME}/g" ./config/appsettings.Docker.json
+sed -i "s/{PG_SUPER_PASSWORD}/${PG_SUPER_PASSWORD}/g" ./config/appsettings.Docker.json
+sed -i "s/{PG_SUPER_USER}/${PG_SUPER_USER}/g" ./config/appsettings.Docker.json
+sed -i "s/{PERSEUS__FILES_MANAGER_HOST}/${PERSEUS__FILES_MANAGER_HOST}/g" ./config/appsettings.Docker.json
+
+cp ./config/appsettings.Docker.json ./appsettings.Docker.json 
+
+dotnet org.ohdsi.cdm.presentation.builderwebapi.dll
