8129064cd28fe852516fd965e6da01e3ab32c338
suwarno ong <48011859+suwarnoong@users.noreply.github.com>
2024-09-18 09:45:03 +0800

    [Integrate-1066] auto grant dashboard viewer role for Entra integration (#343)
    * [Integrate-1066] auto grant dashboard viewer role for Entra integration
    
    * handle null value when dashboard name does not exist

--- services/alp-usermgmt/src/const.ts
+++ services/alp-usermgmt/src/const.ts
@@ -40,5 +40,6 @@ export const CONFIG_KEY = {
 export const IDP_SCOPE_ROLE = {
   TENANT_VIEWER: 'role.tenantviewer',
   SYSTEM_ADMIN: 'role.systemadmin',
-  USER_ADMIN: 'role.useradmin'
+  USER_ADMIN: 'role.useradmin',
+  DASHBOARD_VIEWER: 'role.dashboardviewer'
 }
