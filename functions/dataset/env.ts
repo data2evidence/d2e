4b633afbeb6e0875bcd81fbe8adc25f62111c577
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-10-04 11:14:18 +0800

    fix dataset service route env

--- functions/dataset/env.ts
+++ functions/dataset/env.ts
@@ -1,10 +1,9 @@
-import type { LoggingLevel } from './types.d.ts'
+import type { LoggingLevel } from "./types.d.ts";
 
 const _env = Deno.env.toObject();
 
 export const env = {
   NODE_ENV: _env.NODE_ENV,
-  GATEWAY_WO_PROTOCOL_FQDN: _env.GATEWAY_WO_PROTOCOL_FQDN
-}
-
-//export const services = JSON.parse(env.SERVICE_ROUTES)
+  GATEWAY_WO_PROTOCOL_FQDN: _env.GATEWAY_WO_PROTOCOL_FQDN,
+  SERVICE_ROUTES: _env.SERVICE_ROUTES,
+};
