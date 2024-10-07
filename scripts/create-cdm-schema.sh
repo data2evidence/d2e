cf0781e2ec5b461c1d44cf8bd5053405c57ffb98
alicia-koh <alicia.koh@data4life-asia.care>
2024-10-07 10:54:26 +0800

    Revert "Update create-cdm-schema.sh"
    This reverts commit fb25a0752f40e5a14fd698ce3e369428ff148cc1.

--- scripts/create-cdm-schema.sh
+++ scripts/create-cdm-schema.sh
@@ -4,4 +4,4 @@ DB_CODE=${1:-alpdev_pg}
 SCHEMA_NAME=${2:-cdmdefault}
 CDMVOCAB=${3:-cdmvocab}
 
-docker exec -it alp-dataflow-gen-worker prefect deployment run omop_cdm_plugin/omop_cdm_plugin --param options='{"data_model":"omop5-4","schema_name":'\"${SCHEMA_NAME}\"',"vocab_schema":'\"${CDMVOCAB}\"',"database_code":'\"${DB_CODE}\"',"flow_action_type":"create_cdm_schema"}'
+docker exec -it alp-dataflow-gen-worker prefect deployment run data_management_plugin/data_management_plugin --param options='{"data_model":"omop5-4","schema_name":'\"${SCHEMA_NAME}\"',"vocab_schema":'\"${CDMVOCAB}\"',"database_code":'\"${DB_CODE}\"',"flow_action_type":"create_cdm_schema"}'
