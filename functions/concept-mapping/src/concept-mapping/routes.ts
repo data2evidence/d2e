8584b3238fe8b56b751fe62189539f0bd61b00af
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-10-21 14:36:47 +0800

    enable dto in get

--- functions/concept-mapping/src/concept-mapping/routes.ts
+++ functions/concept-mapping/src/concept-mapping/routes.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import { validationResult, matchedData } from "express-validator";
+import { getCachedbDatabaseFormatProtocolA } from "../../../_shared/alp-base-utils/src/utils";
 import {
   getSourceToConceptMappings,
   saveSourceToConceptMappings,
 } from "./services";
+import { ConceptMappingDto } from "./middleware";
 import { env } from "../env";
 import { DatabaseOptions } from "../types";
 
@@ -15,25 +18,35 @@ export class ConceptMappingRouter {
   }
 
   private registerRoutes() {
-    this.router.get("/", async (req: Request, res: Response) => {
-      this.logger.log("concept mapping route triggered");
-      const user = req.headers["authorization"];
+    this.router.get(
+      "/",
+      ConceptMappingDto(),
+      async (req: Request, res: Response) => {
+        this.logger.log("concept mapping route triggered");
 
-      try {
-        if (!user) {
-          return;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.status(400).json({ errors: errors.array() });
         }
-        const options: DatabaseOptions = {
-          host: env.CACHEDB__HOST,
-          port: env.CACHEDB__PORT,
-          user: user,
-          database: "A|postgresql|a29c196e-4357-4cb9-8a0b-6da85afb0a05",
-        };
-        await getSourceToConceptMappings(options);
-        res.status(200).send("success");
-      } catch (error) {
-        res.status(500).send(error);
+
+        try {
+          const user = req.headers["authorization"]!;
+          const { datasetId, dialect } = matchedData(req, {
+            locations: ["query"],
+          });
+
+          const options: DatabaseOptions = {
+            host: env.CACHEDB__HOST,
+            port: env.CACHEDB__PORT,
+            user: user,
+            database: getCachedbDatabaseFormatProtocolA(dialect, datasetId),
+          };
+          await getSourceToConceptMappings(options);
+          res.status(200).send("success");
+        } catch (error) {
+          res.status(500).send(error);
+        }
       }
-    });
+    );
   }
 }
