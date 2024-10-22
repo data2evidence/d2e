0bc95952f443b90ad1d2e1195dd4df6013af1bad
alicia-koh <alicia.koh@data4life-asia.care>
2024-10-22 13:55:14 +0800

    Update create-cdm-schema.sh

--- scripts/create-cdm-schema.sh
+++ scripts/create-cdm-schema.sh
@@ -4,4 +4,4 @@ DB_CODE=${1:-alpdev_pg}
 SCHEMA_NAME=${2:-cdmdefault}
 CDMVOCAB=${3:-cdmvocab}
 
-docker exec -it alp-dataflow-gen-worker prefect deployment run omop_cdm_plugin/omop_cdm_plugin --param options='{"data_model":"omop5-4","schema_name":'\"${SCHEMA_NAME}\"',"vocab_schema":'\"${CDMVOCAB}\"',"database_code":'\"${DB_CODE}\"',"flow_action_type":"create_seed_schemas_flow"}'
+docker exec -it alp-dataflow-gen-worker prefect deployment run omop_cdm_plugin/omop_cdm_plugin --param options='{"data_model":"omop5-4","schema_name":'\"${SCHEMA_NAME}\"',"vocab_schema":'\"${CDMVOCAB}\"',"database_code":'\"${DB_CODE}\"',"flow_action_type":"create_seed_schemas"}'
