3b703ce08e24256d2166dca7f6897117c9ec908f
Satish <satish.anbazhagan@data4life-asia.care>
2024-10-30 09:36:29 +0800

    Disable https server temporarily

--- services/alp-portal-server/src/main.ts
+++ services/alp-portal-server/src/main.ts
@@ -28,8 +28,8 @@ async function bootstrap() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
   await app.init()
-  http.createServer(server).listen(33002)
-  https.createServer(httpsOptions, server).listen(env.PORT)
+  http.createServer(server).listen(env.PORT)
+  // https.createServer(httpsOptions, server).listen(env.PORT)
   console.log('started https ${env.PORT}')
 }
 bootstrap()
