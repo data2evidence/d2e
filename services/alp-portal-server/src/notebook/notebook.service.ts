a84248077ad9b02ca826b57fc522784d888f9886
Khairul Syazwan <khairul.masri@data4life-asia.care>
2024-11-04 10:44:02 +0800

    Integrate with notebooks service

--- services/alp-portal-server/src/notebook/notebook.service.ts
+++ services/alp-portal-server/src/notebook/notebook.service.ts
@@ -1,36 +1,30 @@
 import { Inject, Injectable, InternalServerErrorException, NotFoundException, Scope } from '@nestjs/common'
 import { REQUEST } from '@nestjs/core'
 import { Request } from 'express'
-import { InjectRepository } from '@nestjs/typeorm'
 import { INotebook, INotebookBaseDto, INotebookUpdateDto } from '../types'
-import { Repository } from 'typeorm'
-import { Notebook } from './entity'
 import { DEFAULT_ERROR_MESSAGE } from '../common/const'
 import { v4 as uuidv4 } from 'uuid'
 import { createLogger } from '../logger'
 import { JwtPayload, decode } from 'jsonwebtoken'
+import { UserArtifactService } from '../user-artifact/user-artifact.service'
+import { ServiceName } from '../user-artifact/enums'
 
 @Injectable({ scope: Scope.REQUEST })
 export class NotebookService {
   private readonly logger = createLogger(this.constructor.name)
   private readonly userId: string
 
-  constructor(
-    @Inject(REQUEST) request: Request,
-    @InjectRepository(Notebook)
-    private notebookRepo: Repository<Notebook>
-  ) {
+  constructor(@Inject(REQUEST) request: Request, private readonly userArtifactService: UserArtifactService) {
     const token = decode(request.headers['authorization'].replace(/bearer /i, '')) as JwtPayload
     this.userId = token.sub
   }
 
-  async getNotebooksByUserId(): Promise<INotebook[]> {
+  async getNotebooksByUserId(): Promise<any[]> {
     try {
-      const userNotebooks = await this.notebookRepo
-        .createQueryBuilder('notebook')
-        .where('notebook.user_id = :id OR notebook.is_shared = :isShared', { id: this.userId, isShared: true })
-        .orderBy('notebook.modified_date', 'DESC')
-        .getMany()
+      const userNotebooks = await this.userArtifactService.getAllUserServiceArtifacts(
+        ServiceName.NOTEBOOKS,
+        this.userId
+      )
 
       return userNotebooks
     } catch (error) {
@@ -41,13 +35,19 @@ export class NotebookService {
 
   async createNotebook(notebookDto: INotebookBaseDto): Promise<INotebook> {
     try {
-      const notebookEntity = this.notebookRepo.create({
-        ...notebookDto,
-        id: uuidv4(),
-        userId: this.userId,
-        isShared: false
+      const notebookEntity = this.addOwner(
+        {
+          ...notebookDto,
+          id: uuidv4(),
+          userId: this.userId,
+          isShared: false
+        },
+        true
+      )
+      await this.userArtifactService.createServiceArtifact({
+        serviceName: ServiceName.NOTEBOOKS,
+        serviceArtifact: notebookEntity
       })
-      await this.notebookRepo.insert(this.addOwner(notebookEntity, true))
       this.logger.info(`Created new notebook ${notebookEntity.name} with id ${notebookEntity.id}`)
       return notebookEntity
     } catch (error) {
@@ -58,15 +58,24 @@ export class NotebookService {
 
   async updateNotebook(notebookUpdateDto: INotebookUpdateDto): Promise<INotebook> {
     try {
-      const notebook = await this.getNotebook(notebookUpdateDto.id)
+      const notebook = await this.userArtifactService.getServiceArtifactById(
+        this.userId,
+        ServiceName.NOTEBOOKS,
+        notebookUpdateDto.id
+      )
+
       if (notebook.userId !== this.userId) {
         this.logger.error('Notebook does not belong to user!')
         throw new InternalServerErrorException('Notebook does not belong to user!')
       }
-      await this.notebookRepo.update(
-        { id: notebookUpdateDto.id, userId: this.userId },
-        this.addOwner(notebookUpdateDto)
-      )
+
+      const updatedServiceEntity = this.addOwner({
+        userId: this.userId,
+        id: notebookUpdateDto.id,
+        serviceArtifact: notebookUpdateDto
+      })
+      await this.userArtifactService.updateServiceArtifact(ServiceName.NOTEBOOKS, updatedServiceEntity)
+
       this.logger.info(`Updated notebook ${notebookUpdateDto.name}`)
       return {
         ...notebookUpdateDto,
@@ -84,7 +93,7 @@ export class NotebookService {
   async deleteNotebook(id: string): Promise<INotebook> {
     try {
       const notebook = await this.getNotebook(id)
-      await this.notebookRepo.delete({ id, userId: this.userId })
+      await this.userArtifactService.deleteServiceArtifact(this.userId, ServiceName.NOTEBOOKS, id)
       return notebook
     } catch (error) {
       this.logger.error(`Error deleting notebook ${id}: ${error}`)
@@ -96,12 +105,7 @@ export class NotebookService {
   }
 
   private async getNotebook(id: string) {
-    const notebook = await this.notebookRepo.findOne({
-      where: [
-        { id, userId: this.userId },
-        { id, isShared: true }
-      ]
-    })
+    const notebook = await this.userArtifactService.getServiceArtifactById(this.userId, ServiceName.NOTEBOOKS, id)
     if (!notebook) {
       throw new NotFoundException(`Notebook with id ${id} not found`)
     }
