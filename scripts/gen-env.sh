e59944901f9373e04f24603a2a03828704222b15
Brian Cornally <93508235+brian-cornally@users.noreply.github.com>
2024-11-06 21:31:46 +0100

    rename & simplify public get-env.sh

---
+++ scripts/gen-env.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# generate dotenv file with passwords, certificates,
+set -o nounset
+set -o errexit
+
+# inputs
+DEFAULT_PASSWORD_LENGTH=30
+DOTENV_FILE_OUT=.env.local
+DOTENV_KEYS_OUT=.env.local.keys
+X509_SUBJECT="/C=SG/O=ALP Dev"
+
+# vars
+GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
+
+# action
+cd $GIT_BASE_DIR
+source scripts/lib.sh
+
+echo DB_CREDENTIALS__INTERNAL__PRIVATE_KEY_PASSPHRASE=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 10) > $DOTENV_FILE_OUT
+set -o allexport; source $DOTENV_FILE_OUT; set +o allexport
+echo DB_CREDENTIALS__INTERNAL__PRIVATE_KEY=\'"$(openssl genpkey -algorithm RSA -aes-256-cbc -pkeyopt rsa_keygen_bits:4096 -pass env:DB_CREDENTIALS__INTERNAL__PRIVATE_KEY_PASSPHRASE -quiet)"\' >> $DOTENV_FILE_OUT
+set -o allexport; source $DOTENV_FILE_OUT; set +o allexport
+echo DB_CREDENTIALS__INTERNAL__PUBLIC_KEY=\'"$(openssl rsa -in <(echo ${DB_CREDENTIALS__INTERNAL__PRIVATE_KEY}) -pubout -passin env:DB_CREDENTIALS__INTERNAL__PRIVATE_KEY_PASSPHRASE)"\' >> $DOTENV_FILE_OUT
+echo DICOM__HEALTH_CHECK_PASSWORD=$(random-password 14) >> $DOTENV_FILE_OUT
+echo DOCKER_TAG_NAME=local >> $DOTENV_FILE_OUT
+echo FHIR__CLIENT_ID=${random-uuid} >> $DOTENV_FILE_OUT
+echo FHIR__CLIENT_SECRET=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo FHIR__CLIENT_SECRET=$(random-password 64) >> $DOTENV_FILE_OUT
+echo LOGTO_API_M2M_CLIENT_ID=$(random-password 21) >> $DOTENV_FILE_OUT
+echo LOGTO_API_M2M_CLIENT_SECRET=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo MEILI_MASTER_KEY=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo MINIO__SECRET_KEY=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo PG_ADMIN_PASSWORD=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo PG_SUPER_PASSWORD=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo PG_WRITE_PASSWORD=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo POSTGRES_TENANT_ADMIN_PASSWORD_PLAIN=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo REDIS_PASSWORD=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+echo STRATEGUS__KEYRING_PASSWORD=$(random-password $DEFAULT_PASSWORD_LENGTH) >> $DOTENV_FILE_OUT
+
+echo LOGTO__CLIENTID_PASSWORD__BASIC_AUTH=$(echo -n "${LOGTO_API_M2M_CLIENT_ID}:${LOGTO_API_M2M_CLIENT_SECRET}" | base64) >> $DOTENV_FILE_OUT
+echo >> $DOTENV_FILE_OUT
+
+# finish
+cat $DOTENV_FILE_OUT | grep = | awk -F= '{print $1}' | grep _ | sort -u > $DOTENV_KEYS_OUT
+wc -l $DOTENV_FILE_OUT $DOTENV_KEYS_OUT | sed '$d'
+echo
