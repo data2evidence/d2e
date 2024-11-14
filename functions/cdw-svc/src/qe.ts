14ca7dbbf6e3e832e6c9bb7eb9a85155b98a0df3
Satish <satish.anbazhagan@data4life-asia.care>
2024-11-14 15:09:40 +0800

    Trex misc fixes 2 (#435)
    * Delete alp-gateway folder
    
    * readd trex schema
    
    * use debug port
    
    * Fix direct URI UI access
    
    * Fix overlap CDW UI URI Access
    
    * CDW Backend fixes
    
    * Fix 403 permissions for concept-set and dataset
    
    * Pull trex always
    
    * Add path for starboard
    
    * Revert changes for authz
    
    * Fix base_port env
    
    * Add healthcheck for prefect and cleanup urls
    
    * Add retry mechanism
    
    * Fix Az deployment and parameterize envs
    
    * Readd dataflow-gen for init
    
    * Update list
    
    * Adapt UI deployment
    
    * Fix cdw validation request

--- functions/cdw-svc/src/qe.ts
+++ functions/cdw-svc/src/qe.ts
@@ -4,22 +4,3 @@
 export { DbMeta } from "./qe/settings/DbMeta";
 export { Settings } from "./qe/settings/Settings";
 export { SettingsFacade } from "./qe/settings/SettingsFacade";
-
-//qe-config
-import { FfhQeConfig } from "./qe/config/config";
-import * as ffhQeConfigDefinition from "./qe/config/configDefinition";
-import * as ffhQeConfigSuggestion from "./qe/config/configSuggestion";
-import * as ffhQeConfigTemplate from "./qe/config/configTemplate";
-import * as ffhQeFormatter from "./qe/config/formatter";
-import { CDWServicesFacade } from "./qe/config/CDWServicesFacade";
-import { ConfigFacade } from "./qe/config/ConfigFacade";
-
-export let qe_config = {
-  FfhQeConfig,
-  ffhQeConfigDefinition,
-  ffhQeConfigSuggestion,
-  ffhQeConfigTemplate,
-  ffhQeFormatter,
-  ConfigFacade,
-  CDWServicesFacade,
-};
