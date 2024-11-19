26d4fe5968d705f6b8e438a2b8c3084259903ab5
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-11-19 13:42:52 +0800

    dummy routes

--- functions/files-manager/src/files-manager/files-manager.router.ts
+++ functions/files-manager/src/files-manager/files-manager.router.ts
@@ -19,7 +19,56 @@ export class FilesManagerRouter {
     this.router.get(
       "/",
       async (_req: Request, res: Response, next: NextFunction) => {
-        res.status(200).send("works");
+        this.logger.info("Download file by userDataId: ");
+
+        try {
+          res.status(200).send("works");
+        } catch (err) {
+          this.logger.error(
+            `Error when downloading file: ${JSON.stringify(err)}`
+          );
+        }
+      }
+    );
+
+    this.router.get(
+      "/",
+      async (_req: Request, res: Response, next: NextFunction) => {
+        this.logger.info(`Save file with username and data-key `);
+
+        try {
+          res.status(200).send("works");
+        } catch (err) {
+          this.logger.error(`Error when saving file: ${JSON.stringify(err)}`);
+        }
+      }
+    );
+
+    this.router.delete(
+      "/",
+      async (_req: Request, res: Response, next: NextFunction) => {
+        this.logger.info(`Delete file by userDataId: `);
+
+        try {
+          res.status(200).send("works");
+        } catch (err) {
+          this.logger.error(`Error when deleting file: ${JSON.stringify(err)}`);
+        }
+      }
+    );
+
+    this.router.get(
+      "/user-data/:userDataId",
+      async (_req: Request, res: Response, next: NextFunction) => {
+        this.logger.info(`get user data by userDataId: `);
+
+        try {
+          res.status(200).send("works");
+        } catch (err) {
+          this.logger.error(
+            `Error when getting user data: ${JSON.stringify(err)}`
+          );
+        }
       }
     );
   }
