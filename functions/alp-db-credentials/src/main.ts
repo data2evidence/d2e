1a004c614ad518bdb82576f4952b03f17a3b4ff2
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-11-25 10:48:31 +0800

    remove unneeded code

--- functions/alp-db-credentials/src/main.ts
+++ functions/alp-db-credentials/src/main.ts
@@ -6,9 +6,6 @@ import { createLogger } from './logger'
 import { env } from './env'
 import { Routes } from './routes'
 import { setupReqContext, healthCheck } from './common/middleware'
-import { runMigrations } from './common/data-source/db-migration'
-import dataSource from './common/data-source/data-source'
-import { loadLocalDatabaseCredentials } from './local-setup'
 import https from 'https'
 
 const PORT = env.PORT || 8000
@@ -41,23 +38,7 @@ export class App {
     })
   }
 
-  private async initialiseDataSource() {
-    try {
-      await dataSource.initialize()
-      logger.info('Datasource is initialised')
-      await runMigrations()
-      if (env.NODE_ENV === 'development') {
-        loadLocalDatabaseCredentials(logger)
-      }
-    } catch (error) {
-      //console.log(env.PG_HOST)
-      logger.error(`Error while initialising datasource: ${error}`)
-      process.exit(0)
-    }
-  }
-
   async start() {
-    await this.initialiseDataSource()
     this.registerMiddlewares()
     this.registerRoutes()
     this.registerValidatorContainer()
@@ -75,5 +56,3 @@ export class App {
     logger.info(`🚀 ALP DB Credentials Manager started successfully!. Server listening on port ${PORT}`)
   }
 }
-
-//new App().start()
