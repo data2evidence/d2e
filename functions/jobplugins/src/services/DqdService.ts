1d6e264016c20a756e19052641a7f30c5d0a957b
hengxian-jiang <155619404+hengxian-jiang@users.noreply.github.com>
2024-11-28 11:37:08 +0800

    [Integrate-1291] Fix dc dqd flow creation endpoints (#451)
    * Fix Dqd run error
    
    * Fix dc create flow run

--- functions/jobplugins/src/services/DqdService.ts
+++ functions/jobplugins/src/services/DqdService.ts
@@ -1,7 +1,12 @@
 import { AnalyticsSvcAPI } from "../api/AnalyticsAPI.ts";
 import { PortalServerAPI } from "../api/PortalServerAPI.ts";
 import { PrefectAPI } from "../api/PrefectAPI.ts";
-import { DATA_QUALITY_DOMAINS, PrefectTagNames } from "../const.ts";
+import {
+  DATA_QUALITY_DOMAINS,
+  PrefectDeploymentName,
+  PrefectFlowName,
+  PrefectTagNames,
+} from "../const.ts";
 import {
   IDataCharacterizationResult,
   IDataQualityResult,
@@ -122,8 +127,6 @@ export class DqdService {
     const analyticsSvcApi = new AnalyticsSvcAPI(token);
 
     const {
-      flowName,
-      deploymentName,
       datasetId,
       comment,
       vocabSchemaName,
@@ -159,8 +162,8 @@ export class DqdService {
 
     return await prefectApi.createFlowRun(
       name,
-      deploymentName,
-      flowName,
+      PrefectDeploymentName.DQD,
+      PrefectFlowName.DQD,
       parameters
     );
   }
