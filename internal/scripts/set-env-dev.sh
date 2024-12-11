ad106850149d5ed0178f804b1cca49ab782a2f16
Brian Cornally <93508235+brian-cornally@users.noreply.github.com>
2024-12-11 09:02:09 +0100

    fix path

--- internal/scripts/set-env-dev.sh
+++ internal/scripts/set-env-dev.sh
@@ -5,17 +5,19 @@
 
 # inputs
 ENV_TYPE=${ENV_TYPE:-local}
-RELATIVE_PATH__PLUGINS_REPO=${RELATIVE_PATH__PLUGINS_REPO:-../d2e-plugins}
-RELATIVE_PATH__UI_REPO=${RELATIVE_PATH__UI_REPO:-../d2e-ui}
+# relative to $GIT_BASE_DIR/internal
+RELATIVE_PATH__PLUGINS_REPO=${RELATIVE_PATH__PLUGINS_REPO:-../../d2e-plugins}
+RELATIVE_PATH__UI_REPO=${RELATIVE_PATH__UI_REPO:-../../d2e-ui}
 
 # validate
 [ $ENV_TYPE != local ] && [ $ENV_TYPE != remote ] && echo FATAL: ENV_TYPE=local|remote && exit 1
 
 # vars
-export GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
 DOTENV_FILE_OUT=.env.$ENV_TYPE
+export GIT_BASE_DIR="$(git rev-parse --show-toplevel)"
 
-# \n at the end of the file only if it doesn’t already end with a newline.
+# action
+# ensure \n at the end of the file only if it doesn’t already end with a newline.
 echo >> $DOTENV_FILE_OUT && sed -i.bak '/^$/d' $DOTENV_FILE_OUT # remove empty lines
 
 # remove existing vars from dotenv
@@ -24,5 +26,5 @@ for VAR_NAME in GIT__COMMIT_ID__UI GIT__COMMIT_ID__PLUGINS PREFECT_DOCKER_VOLUME
 echo . appending to $DOTENV_FILE_OUT ...
 echo GIT__COMMIT_ID__PLUGINS=$(git -C ${RELATIVE_PATH__PLUGINS_REPO} rev-parse HEAD || echo main) | tee -a $DOTENV_FILE_OUT
 echo GIT__COMMIT_ID__UI=$(git -C $RELATIVE_PATH__UI_REPO rev-parse HEAD || echo develop) | tee -a $DOTENV_FILE_OUT
-echo GIT_BASE_DIR=$(git rev-parse --show-toplevel 2> /dev/null) | tee -a $DOTENV_FILE_OUT
+echo GIT_BASE_DIR="${GIT_BASE_DIR}" | tee -a $DOTENV_FILE_OUT
 echo PREFECT_DOCKER_VOLUMES='["alp_duckdb-data-1:/app/duckdb_data", "alp_cdw-config-duckdb-data-1:/app/cdw-config/duckdb_data", "alp_r-libs:/home/docker/plugins/R/site-library", "alp_fhir-schema-file-1:/home/docker/fhir", "$GIT_BASE_DIR/cache/synpuf1k:/app/synpuf1k", "${GIT_BASE_DIR}/cache/vocab:/app/vocab"]' | envsubst | awk -F= '{print $1"=\047" $2 "\047"}' | tee -a $DOTENV_FILE_OUT
