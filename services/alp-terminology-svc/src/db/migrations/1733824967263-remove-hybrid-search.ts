2c10b1fcf41cfda57bb0cab8cb857a77931d0307
brandantck <brandan.tanck@gmail.com>
2024-12-16 15:15:48 +0800

    change rollback to create table if not exists

--- services/alp-terminology-svc/src/db/migrations/1733824967263-remove-hybrid-search.ts
+++ services/alp-terminology-svc/src/db/migrations/1733824967263-remove-hybrid-search.ts
@@ -9,7 +9,7 @@ export class RemoveHybridSearch1733824967263 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DROP TABLE IF EXISTS "terminology"."hybrid_search_config"; CREATE TABLE "terminology"."hybrid_search_config" ("id" SERIAL NOT NULL, "is_enabled" boolean NOT NULL, "semantic_ratio" real NOT NULL, "model" character varying NOT NULL, "created_by" character varying NOT NULL, "created_date" TIMESTAMP NOT NULL DEFAULT now(), "modified_by" character varying NOT NULL, "modified_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_442afda502080d54e544aec329c" PRIMARY KEY ("id"))`,
+      `CREATE TABLE IF NOT EXISTS "terminology"."hybrid_search_config" ("id" SERIAL NOT NULL, "is_enabled" boolean NOT NULL, "semantic_ratio" real NOT NULL, "model" character varying NOT NULL, "created_by" character varying NOT NULL, "created_date" TIMESTAMP NOT NULL DEFAULT now(), "modified_by" character varying NOT NULL, "modified_date" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_442afda502080d54e544aec329c" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "terminology"."hybrid_search_config" ADD "source" character varying`,
