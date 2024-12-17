04b9743ff0b06744d79bcaac747d0d56836072dd
brandantck <brandan.tanck@gmail.com>
2024-12-17 15:14:11 +0800

    add APIModule to concept-set

--- services/alp-terminology-svc/src/module/concept-set/concept-set.module.ts
+++ services/alp-terminology-svc/src/module/concept-set/concept-set.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConceptSetService } from './concept-set.service';
 import { ConceptSetController } from './concept-set.controller';
 import { CachedbModule } from 'src/module/cachedb/cachedb.module';
+import { APIModule } from 'src/api/api.module';
 @Module({
-  imports: [CachedbModule],
+  imports: [CachedbModule, APIModule],
   controllers: [ConceptSetController],
   exports: [ConceptSetModule],
   providers: [ConceptSetService],
