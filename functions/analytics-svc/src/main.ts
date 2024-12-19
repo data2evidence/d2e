d3d62d9a69927417081bc3294bea4664993d16d5
Satish <satish.anbazhagan@data4life-asia.care>
2024-12-19 23:32:33 +0800

    Implement Authc Hana jwt (#486)
    * Add new env to enable Hana JWT
    
    * Modify user object
    
    * cleanup
    
    * Inject JWT Hana credentials
    
    * Clean / comment out verbose logs
    
    * Improve error handling
    
    * Handle dataset creation scenario for HANA JWT Authc

--- functions/analytics-svc/src/main.ts
+++ functions/analytics-svc/src/main.ts
@@ -81,14 +81,13 @@ const initRoutes = async (app: express.Application) => {
     // Calls Alp-Portal for studies db metadata and cache it
     // Ignore Alp-Portal check for readiness probe check
     app.use(async (req: IMRIRequest, res, next) => {
-        //console.log("AAAAAA")
-        log.debug(
-            `🚀 ~ file: main.ts ~ line 107 ~ app.use ~ req.headers: ${JSON.stringify(
-                req.headers,
-                null,
-                2
-            )}`
-        );
+        // log.debug(
+        //     `🚀 ~ file: main.ts ~ line 107 ~ app.use ~ req.headers: ${JSON.stringify(
+        //         req.headers,
+        //         null,
+        //         2
+        //     )}`
+        // );
         const hasExpiredStudiesDbMetadataCache = (): boolean => {
             if (!studiesDbMetadata) {
                 return true;
@@ -151,11 +150,11 @@ const initRoutes = async (app: express.Application) => {
                 let userObj: User;
                 try {
                     userObj = getUser(req);
-                    log.debug(
-                        `req.headers: ${JSON.stringify(req.headers)}\n
-                            currentUser: ${JSON.stringify(userObj)}\n
-                            url is: ${req.url}`
-                    );
+                    // log.debug(
+                    //     `req.headers: ${JSON.stringify(req.headers)}\n
+                    //         currentUser: ${JSON.stringify(userObj)}\n
+                    //         url is: ${req.url}`
+                    // );
                 } catch (err) {
                     log.debug(`No user found in request:${err.stack}`);
                 }
@@ -674,6 +673,17 @@ const getDBConnections = async ({
             delete analyticsCredentials.ca;
             delete analyticsCredentials.pfx;
         }
+
+        if (analyticsCredentials.dialect === DB.HANA && env.USE_HANA_JWT_AUTHC === "true") {
+            delete analyticsCredentials.user
+            delete analyticsCredentials.password
+            if (userObj.thirdPartyToken) {
+                analyticsCredentials["token"] = userObj.thirdPartyToken;
+            } else {
+                throw new Error("Intermediary IDP token doesnt exist for HANA JWT Authentication!");
+            }
+        }
+
         analyticsConnectionPromise =
             dbConnectionUtil.DBConnectionUtil.getDBConnection({
                 credentials: analyticsCredentials,
