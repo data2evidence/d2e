b41eab3881133bf87437a05e6ca3c1e5fc20946e
Satish <satish.anbazhagan@data4life-asia.care>
2024-12-19 15:31:39 +0800

    Always use write connection with pg from duckdb

--- services/cachedb/src/buenavista/database.py
+++ services/cachedb/src/buenavista/database.py
@@ -207,7 +207,7 @@ def get_db_connection(clients: CachedbDatabaseClients, dialect: str, connection_
 
 
 def _attach_direct_postgres_connection_for_duckdb(db: duckdb.DuckDBPyConnection, database_code: str, connection_type: ConnectionTypes):
-    duckdb_pg_connection_type = "(TYPE postgres, READ_ONLY)" if connection_type == ConnectionTypes.READ else "(TYPE postgres)"
+    duckdb_pg_connection_type = "(TYPE postgres)"
     print("Attaching postgres as direct connection ")
     conn_details = extract_db_credentials(database_code)
 
