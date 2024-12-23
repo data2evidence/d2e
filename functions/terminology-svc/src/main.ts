d369a9eb98bdaefb4867e3d7f4f680f57f16744a
jerome-ng <107386741+jerome-ng@users.noreply.github.com>
2024-12-23 13:26:18 +0800

    1342 move terminology to trex (#499)
    * Add function terminology boiler plate
    
    * Add router
    
    * add some terminology envs
    
    * Add controller
    
    * Add placeholder concept set routes
    
    * Add routes placeholders for fhir and concepts
    
    * Implement Get concept sets
    
    * Set up filter options
    
    * Fix filter options return value
    
    * Fix route for $expand
    
    * add updateOpenApi.ts
    
    * move stuff around
    
    * Get concepts page to work
    
    * make $translate work
    
    * add validation for getConcepts
    
    * add validation for concept
    
    * add searchByName
    
    * add searchConceptById
    
    * add searchConceptByCode
    
    * add recommended/list
    
    * add getConceptHierarchy
    
    * add getStandardConcepts
    
    * add createConceptSet
    
    * add getConceptSet and updateConceptSet
    
    * add removeConceptSet
    
    * add getIncludedConcepts
    
    * clean up
    
    * clean up
    
    * add deno tasks
    
    * update openapi descriptions
    
    * remove 204
    
    * Add dummy responses to openapi
    
    * update openapi
    
    * fix dependencies
    
    * remove terminology from services
    
    * empty commit

---
+++ functions/terminology-svc/src/main.ts
@@ -0,0 +1,29 @@
+// @ts-types="npm:@types/express"
+import express, { NextFunction, Response, Request } from "express";
+import { z } from "zod";
+import "./env.ts"; // Ensure env var validation is done on start
+import { router } from "./routes.ts";
+
+const main = () => {
+  const app = express();
+  app.use(express.json());
+
+  app.use(router);
+
+  app.use((e: Error, _req: Request, res: Response, _next: NextFunction) => {
+    if (e instanceof z.ZodError) {
+      res.status(400).json(e.issues);
+      return;
+    }
+    console.error(e.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
+
+  const port = 3000;
+
+  app.listen(port, () => {
+    console.log(`Terminology service is running at http://localhost:${port}`);
+  });
+};
+
+export { main };
