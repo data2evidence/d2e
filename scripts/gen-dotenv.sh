ed51d6361d58ab19d22966fbcc35af5a883dfd5a
santan maddi <saisantan2004@gmail.com>
2024-12-23 12:46:21 +0800

    WIP: check scripts for OS user where database_credentials are encrypted

--- scripts/gen-dotenv.sh
+++ scripts/gen-dotenv.sh
@@ -41,10 +41,10 @@ echo set-openssl ...
 # https://www.openssl.org/docs/man3.0/man1/openssl-passphrase-options.html
 function set-openssl {
     echo INFO set DB_CREDENTIALS__INTERNAL__PRIVATE_KEY_PASSPHRASE DB_CREDENTIALS__INTERNAL__PRIVATE_KEY DB_CREDENTIALS__INTERNAL__PUBLIC_KEY ...
-    export PASSPHRASE=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c $passphrase_length)
-    export PRIVATE_KEY=$(openssl genpkey -algorithm RSA -aes-256-cbc -pkeyopt rsa_keygen_bits:4096 -pass env:PASSPHRASE -quiet)
-    export PUBLIC_KEY=$(echo "${PRIVATE_KEY}" | openssl rsa -pubout -passin env:PASSPHRASE)
-    echo DB_CREDENTIALS__INTERNAL__PRIVATE_KEY_PASSPHRASE=\"${PASSPHRASE}\" >> $tmp_file
+    # TODO: below fails due to removal of passphrase
+    export PRIVATE_KEY=$(openssl genpkey  -algorithm RSA -aes-256-cbc -pkeyopt rsa_keygen_bits:4096 -quiet)
+    export PUBLIC_KEY=$(echo "${PRIVATE_KEY}" | openssl rsa -pubout -passin)
+    
     echo DB_CREDENTIALS__INTERNAL__PRIVATE_KEY=\'"${PRIVATE_KEY}"\' >> $tmp_file
     echo DB_CREDENTIALS__INTERNAL__PUBLIC_KEY=\'"${PUBLIC_KEY}"\' >> $tmp_file
 }
