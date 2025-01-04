a7704f2456ad9a7aa481bca2ccf09cc60067b6b3
hengxian-jiang <hengxian.jiang@u.nus.edu>
2025-01-04 14:02:38 +0800

    Extra log

--- functions/alp-usermgmt/src/middlewares/permitted-tenant-check.ts
+++ functions/alp-usermgmt/src/middlewares/permitted-tenant-check.ts
@@ -42,8 +42,8 @@ export const permittedTenantCheck =
       }
 
       const authorizedTenant = allTenantIds.includes(tenantId)
-      console.log(`authorizedTenant: ${authorizedTenant}`)
-      console.log(`tenantId: ${tenantId}`)
+      logger.info(`authorizedTenant: ${authorizedTenant}`)
+      logger.info(`tenantId: ${tenantId}`)
       if (!authorizedTenant) {
         logger.warn(`SECURITY INCIDENT: User (${ctxUserId}) does not belong to tenant (${tenantId})`)
         return res.status(403).send('You do not have enough privileges to manage this tenant')
