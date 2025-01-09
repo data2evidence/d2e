b2ff5c83d5d499130ddc121eb74b68866e3faccf
santan maddi <saisantan2004@gmail.com>
2025-01-09 17:27:37 +0800

    cleanup

--- functions/jobplugins/src/services/DqdService.ts
+++ functions/jobplugins/src/services/DqdService.ts
@@ -353,25 +353,6 @@ export class DqdService {
     return false;
   };
 
-  private regexMatcher(result: any[]): string[] {
-    const regex = /\[s3:\/\/[^)]+\]/;
-    return result
-      .map((item) => item.description.match(regex))
-      .filter((match) => match)
-      .flat();
-  }
-
-  private extractRelativePath(path: string): string | null {
-    const prefix = "s3://flows/";
-    const start = path.indexOf(prefix);
-    if (start === -1) return null;
-
-    const end = path.indexOf(")", start);
-    if (end === -1) return path.substring(start + prefix.length);
-
-    return path.substring(start + prefix.length, end);
-  }
-
   private async getReleaseDate(
     releaseId: string,
     portalServerApi: PortalServerAPI
