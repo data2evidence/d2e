7a29fbf2041741edcf22b0329cd45470d3b8181a
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-12 16:34:34 +0800

    rename npm cli pkg (#575)

---
+++ scripts/cli-login.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source .env
+docker login -u $GH_USERNAME -p $GH_TOKEN
--- scripts/cli-start.sh
+++ scripts/cli-start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 script_full_path=$(dirname "$0")
-node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/setup/
+node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/cli/
 export CADDY__CONFIG=$node_modules_path/deploy/caddy-config
 export ENV_TYPE=${ENV_TYPE:-remote}
 
