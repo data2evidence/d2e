a82c4463c3539132f37341caecb3bcba3397a3b4
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-12 21:28:19 +0800

    fix scripts

--- scripts/cli-genenv.sh
+++ scripts/cli-genenv.sh
@@ -2,6 +2,9 @@
 script_full_path=$(dirname "$0")
 node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/cli/
 
-ENV_EXAMPLE=$node_modules_path/env.example
+export ENV_EXAMPLE=$node_modules_path/env.example
+export GIT_BASE_DIR=.
+export ENVFILE=.env
+export ENV_TYPE=${ENV_TYPE:-remote}
 
-ENV_TYPE=${ENV_TYPE:-remote} $node_modules_path/scripts/gen-dotenv.sh && $node_modules_path/scripts/gen-tls.sh && $node_modules_path/scripts/gen-resource-limits.sh
+$node_modules_path/scripts/gen-dotenv.sh && $node_modules_path/scripts/gen-tls.sh && $node_modules_path/scripts/gen-resource-limits.sh
