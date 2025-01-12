c5b056fb9b6045cd871df487fbaac5ce06ef20ea
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-12 14:45:57 +0800

    add start scripts

---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+script_full_path=$(dirname "$0")
+node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/setup/
+export CADDY__CONFIG=$node_modules_path/deploy/caddy-config
+export ENV_TYPE=${ENV_TYPE:-remote}
+
+docker compose --file $node_modules_path/docker-compose.yml --env-file .env up alp-minerva-postgres alp-logto --wait &&
+sleep 10 &&
+docker compose --file $node_modules_path/docker-compose.yml --env-file .env up alp-logto-post-init
---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+script_full_path=$(dirname "$0")
+node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/setup/
+export CADDY__CONFIG=$node_modules_path/deploy/caddy-config
+export ENV_TYPE=${ENV_TYPE:-remote}
+
+docker compose --file $node_modules_path/docker-compose.yml --env-file .env up
