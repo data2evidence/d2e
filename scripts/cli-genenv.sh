9e0cbc36607f70fa03df668afa1eef8c45879082
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-12 16:24:45 +0800

    rename npm cli pkg

--- scripts/cli-genenv.sh
+++ scripts/cli-genenv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 script_full_path=$(dirname "$0")
-node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/setup/
+node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/cli/
 
 ENV_TYPE=${ENV_TYPE:-remote} $node_modules_path/scripts/gen-dotenv.sh && $node_modules_path/scripts/gen-tls.sh && $node_modules_path/scripts/gen-resource-limits.sh
