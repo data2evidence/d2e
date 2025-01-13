efffae994da20c8cd80221dafe6fb26d49800839
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-13 21:15:06 +0800

    Copy caddycfg to local dir

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -3,7 +3,7 @@
 cmd=${@: -1}
 script_full_path=$(dirname "$0")
 node_modules_path=$script_full_path/../lib/node_modules/@data2evidence/cli/
-export CADDY__CONFIG=$node_modules_path/deploy/caddy-config
+export CADDY__CONFIG=./deploy/caddy-config
 export ENV_TYPE=${ENV_TYPE:-remote}
 
 export ENV_EXAMPLE=$node_modules_path/env.example
@@ -28,6 +28,7 @@ case $cmd in
         esac
         ;;
     init)
+        cp -a $node_modules_path/deploy ./deploy &&
         docker compose --file $node_modules_path/docker-compose.yml --env-file .env up alp-minerva-postgres alp-logto --wait &&
         sleep 10 &&
         docker compose --file $node_modules_path/docker-compose.yml --env-file .env up alp-logto-post-init
