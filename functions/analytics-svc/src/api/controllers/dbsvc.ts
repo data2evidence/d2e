d950a6cc19345e31d25cbbdfaa5caf5e5aab77ec
alicia-koh <alicia.koh@data4life-asia.care>
2025-01-13 14:05:14 +0800

    Remove handling of prefix in cdmversion

--- functions/analytics-svc/src/api/controllers/dbsvc.ts
+++ functions/analytics-svc/src/api/controllers/dbsvc.ts
@@ -24,11 +24,6 @@ export async function getCDMVersion(req, res, next) {
 
         let cdmVersion = await dbDao.getCDMVersion(dbConnection, schemaName);
 
-        // For datasets with prefix in cdm version e.g. v5.3.1
-        if (cdmVersion && (cdmVersion[0] === 'v' || cdmVersion[0] === 'V')) {
-            cdmVersion = cdmVersion.substring(1); // Remove the first char
-          }
-
         let hanaKey = "CDM_VERSION";
         let cdmVersionKey =
             dialect === config.DB.HANA
