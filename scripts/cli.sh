569e6baae4a4d1962021dbf04b9b9b3488c347ff
Peter Hoffmann <954078+p-hoffmann@users.noreply.github.com>
2025-01-13 21:29:22 +0800

    Update cli.sh

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -14,14 +14,30 @@ case $cmd in
     start)
         docker compose --file $node_modules_path/docker-compose.yml --env-file .env up --wait
         ;;
+    startdemo)
+        docker compose --file $node_modules_path/docker-compose.yml --file docker-compose-atlas.yml --env-file .env up --wait
+        ;;
     stop)
         docker compose --file $node_modules_path/docker-compose.yml --env-file .env stop
+        ;;
+    stopdemo)
+        docker compose --file $node_modules_path/docker-compose.yml --file docker-compose-atlas.yml --env-file .env stop
         ;;
     clean)
         read -p "This action will delete all docker containers and volumnes. Continue (y/n)?" choice
         case "$choice" in 
             y|Y)
                 docker compose --file $node_modules_path/docker-compose.yml --env-file .env down --volumes --remove-orphans
+                ;;
+            *)
+                echo "Aborting";;
+        esac
+        ;;
+    cleandemo)
+        read -p "This action will delete all docker containers and volumnes. Continue (y/n)?" choice
+        case "$choice" in 
+            y|Y)
+                docker compose --file $node_modules_path/docker-compose.yml --file docker-compose-atlas.yml --env-file .env down --volumes --remove-orphans
                 ;;
             *)
                 echo "Aborting";;
@@ -56,6 +72,9 @@ case $cmd in
         echo "  start       Starts d2e services"
         echo "  stop        Stops d2e services"
         echo "  clean       Removes d2e docker containers and volumnes"
+        echo "  startdemo   Starts d2e services incl. demo database"
+        echo "  stopdemo    Stops d2e services incl. demo database"
+        echo "  cleandemo   Removes d2e docker containers and volumnes incl. demo database"
         ;;
 esac
     
